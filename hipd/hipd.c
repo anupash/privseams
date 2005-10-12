9496284a824c14ac80ac18531b0d9a63935e3986
Miika Komu <miika@iki.fi>
2005-10-12 15:41:38 +0000

    Put all shared files into a separate library.

--- hipd/hipd.c
+++ hipd/hipd.c
@@ -10,24 +10,7 @@
  * GNU General Public License for more details.
  */
 
-#include <signal.h>     /* signal() */
-#include <stdio.h>      /* stderr and others */
-#include <errno.h>      /* errno */
-#include <unistd.h>
-#include <fcntl.h>
-#include <linux/netlink.h>      /* get_my_addresses() support   */
-#include <linux/rtnetlink.h>    /* get_my_addresses() support   */
-
-#include "hip.h"
 #include "hipd.h"
-#include "crypto.h"
-#include "cookie.h"
-#include "workqueue.h"
-//#include "debug.h"
-#include "netdev.h"
-#ifdef CONFIG_HIP_HI3
-#include "i3_client_api.h"
-#endif
 
 struct hip_nl_handle nl_khipd;
 struct hip_nl_handle nl_ifaddr;
--- hipd/hipd.h
+++ hipd/hipd.h
@@ -1,6 +1,24 @@
 #ifndef HIPD_H
 #define HIPD_H
 
+#include <signal.h>     /* signal() */
+#include <stdio.h>      /* stderr and others */
+#include <errno.h>      /* errno */
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/netlink.h>      /* get_my_addresses() support   */
+#include <linux/rtnetlink.h>    /* get_my_addresses() support   */
+
+#include "hip.h"
+#include "crypto.h"
+#include "cookie.h"
+#include "workqueue.h"
+//#include "debug.h"
+#include "netdev.h"
+#ifdef CONFIG_HIP_HI3
+#include "i3_client_api.h"
+#endif
+
 extern struct hip_nl_handle nl_khipd;
 extern time_t load_time;
 
