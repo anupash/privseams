c8766aba1476d7588b43796d0175c370afac2e43
Miika Komu <miika@iki.fi>
2005-10-13 07:30:38 +0000

    Fixed some compilation problems.

--- hipd/crypto.h
+++ hipd/crypto.h
@@ -1,12 +1,12 @@
-#ifndef HIP_CRYPTO_H
-#define HIP_CRYPTO_H
+#ifndef HIPD_CRYPTO_H
+#define HIPD_CRYPTO_H
 
-#include "hip.h"
-//#include "debug.h"
+#if HAVE_CONFIG_H
+#  include <config.h>
+#endif
 
 #include <sys/time.h>
 #include <time.h>
-#include "hip.h"
 #include <openssl/dsa.h>
 #include <openssl/rsa.h>
 #include <openssl/dh.h>
@@ -15,8 +15,16 @@
 #include <openssl/bio.h>
 #include <openssl/pem.h>
 #include <openssl/err.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <asm/types.h>
+#include <string.h>
+#include <netinet/in.h>
 
 #include "hidb.h"
+#include "hip.h"
+#include "debug.h"
 
 #define HIP_DSA_SIG_SIZE 41 /* T(1) + R(20) + S(20)  from RFC 2536 */
 #define DSA_PRIV 20 /* Size in bytes of DSA private key and Q value */
@@ -32,6 +40,32 @@
 #define HIP_DH_OAKLEY_18              6 /* 8192-bit MODP group */
 #define HIP_DEFAULT_DH_GROUP_ID       HIP_DH_OAKLEY_5
 #define HIP_MAX_DH_GROUP_ID 7 
+
+#define HIP_MAX_DSA_KEY_LEN 4096
+#define HIP_MAX_RSA_KEY_LEN 4096
+#define HIP_MAX_DH_GROUP_ID 7 
+
+#define DSA_KEY_DEFAULT_BITS    (128 * 8)
+#define RSA_KEY_DEFAULT_BITS    1024
+
+#define DEFAULT_CONFIG_DIR        "/etc/hip"
+#define DEFAULT_CONFIG_DIR_MODE   0755
+#define DEFAULT_HOST_DSA_KEY_FILE_BASE "hip_host_dsa_key"
+#define DEFAULT_HOST_RSA_KEY_FILE_BASE "hip_host_rsa_key"
+#define DEFAULT_PUB_FILE_SUFFIX ".pub"
+
+#define DEFAULT_PUB_HI_FILE_NAME_SUFFIX "_pub"
+#define DEFAULT_ANON_HI_FILE_NAME_SUFFIX "_anon"
+
+/* Only one crypto-filefmt supported */
+#define HIP_KEYFILE_FMT_HIP_PEM 1
+
+#ifdef CONFIG_HIP_DEBUG
+void keygen_callback(int a, int b, void* arg);
+#define KEYGEN_CALLBACK keygen_callback
+#else
+#define KEYGEN_CALLBACK NULL
+#endif
 
 int hip_build_digest(const int type, const void *in, int in_len, void *out);
 
@@ -53,4 +87,14 @@ void hip_free_dh(DH *target);
 u16 hip_get_dh_size(u8 hip_dh_group_type);
 void get_random_bytes(void *buf, int n);
 
-#endif /* HIP_CRYPTO_H */
+int dsa_to_hit(DSA *dsa_key, char *dsa, int type, struct in6_addr *hit);
+int rsa_to_hit(RSA *rsa_key, char *rsa, int type, struct in6_addr *hit);
+int dsa_to_dns_key_rr(DSA *dsa, unsigned char **buf);
+
+DSA *create_dsa_key(int bits);
+RSA *create_rsa_key(int bits);
+int save_dsa_private_key(const char *filenamebase, DSA *dsa);
+int load_dsa_private_key(const char *filenamebase, DSA **dsa);
+int load_dsa_public_key(const char *filenamebase, DSA **dsa);
+
+#endif /* HIPD_CRYPTO_H */
