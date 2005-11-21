94c4dda4f7d9dce6913d5e68e982689805a29050
Miika Komu <miika@iki.fi>
2005-11-21 13:59:32 +0000

    Receiving of I1 works now, but crashes :) Receiving of I1 works now, but crashes:
    debug(input.c:1632@hip_handle_i1): hip_handle_i1
    debug(output.c:223@hip_xmit_r1): 
    Segmentation fault
    
    The reason for the segfault is that the code currently tries to receive 
    the I1 with "recvfrom" system call. It returns the address of the peer, 
    but not the local address. However, the local address is needed for the 
    packet handling code.
    
    Alternatives on how to fix this:
    
    * Find a socket option for quering the receiving address.
    * Rewrite the msg receiving based on recvmsg interface.
    * We try to match a local address based the largest prefix of the peer 
      address. This heuristic may not always return the same address as 
      in the I1 packet.

--- hipd/hipd.c
+++ hipd/hipd.c
@@ -133,6 +133,7 @@ int main(int argc, char *argv[]) {
 	struct hip_work_order ping;
 
 	struct hip_common *user_msg = NULL;
+	struct msghdr sock_msg;
 	struct sockaddr_un daemon_addr;
 
 	/* Parse command-line options */
@@ -283,12 +284,22 @@ int main(int argc, char *argv[]) {
 			/* idle cycle - select() timeout */
 			_HIP_DEBUG("Idle\n");
 		} else if (FD_ISSET(hip_raw_sock, &read_fdset)) {
-			/* XX FIX: read an IPv6(HIP) message from the raw
-			   socket, and the IP addresses and IP header to
-			   hip_receive_control_packet() */
-			_HIP_DEBUG("Here we are\n");
-			//err = hip_receive_control_packet(msg, saddr, daddr);
-			//return -1;
+			struct sockaddr_in6 peer;
+			int len;
+			socklen_t socklen;
+                        /* Something on HIP v6 socket */
+			hip_msg_init(user_msg);
+                        len = recvfrom(hip_raw_sock, user_msg, HIP_MAX_PACKET,
+				       0, (struct sockaddr *) &peer,
+				       &socklen);
+			
+                        if (len <= 0) {
+				HIP_ERROR("Receiving error or icmpv6?\n");
+                        } else {
+				err = hip_receive_control_packet(user_msg,
+								 NULL,
+								 &peer);
+                        }
 		} else if (FD_ISSET(hip_user_sock, &read_fdset)) {
 			int n;
 			socklen_t alen;
