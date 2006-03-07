ec087537cb0f9c0c68f230b31583b2f57f18563b
Miika Komu <miika@iki.fi>
2006-03-07 22:28:17 +0000

    Wrote the code for the previous skeletons.

--- test/performance/cookieperf.sh
+++ test/performance/cookieperf.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-# XX TODO
+export PATH=$PATH:..
+
+for K in `seq 1 40`
+do
+  LOGFILE="logs/cookieperf-${K}"
+  rm -f $LOGFILE
+  for REPEAT in `seq 1 30`
+    do
+    cookietest $K 2>&1|grep "puzzle solved"|tee -a $LOGFILE
+  done
+done
--- test/performance/keygenperf.sh
+++ test/performance/keygenperf.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-# XX TODO
+export PATH=$PATH:..
+
+for ALGO in dsa rsa
+do
+  for BITS in `seq 256 256 4096`
+  do
+    LOGFILE="logs/keygentest-${ALGO}-${BITS}"
+    rm -f $LOGFILE
+    for REPEAT in `seq 1 30`
+    do
+      keygentest $ALGO $BITS 2>&1|grep "created"|tee -a $LOGFILE
+    done
+  done
+done
