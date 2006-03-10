0f05bf9f7978c7d4856f18b5d17f048a4fcb1757
Miika Komu <miika@iki.fi>
2006-03-10 15:18:10 +0000

    Added more scripts

--- test/performance/cookiestats.sh
+++ test/performance/cookiestats.sh
@@ -8,5 +8,6 @@ rm -f $LOG
 
 for K in `seq 1 30`
 do
-  stats.pl 95 type '.*(puzzle)\s+solved\s+in\s+(\S+)\s*' <logs/cookieperf-${K} | tail -1| tee -a $LOG
+  echo -n "$K" >>$LOG
+  stats.pl 95 type '.*(puzzle)\s+solved\s+in\s+(\S+)\s*' <logs/cookieperf-${K} | tail -1 | tee -a $LOG
 done
--- test/performance/keygenplot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-XX TODO
