735fb27332a2dc5d263a6fd5464eb3fdc7978992
Miika Komu <miika@iki.fi>
2006-03-22 21:04:08 +0000

    Potential bugfix for DH TLV size

--- libhiptool/builder.c
+++ libhiptool/builder.c
@@ -1695,7 +1695,7 @@ int hip_build_param_diffie_hellman_contents(struct hip_common *msg,
 	diffie_hellman.group_id = group_id; /* 1 byte, no htons() */
 
 	err = hip_build_generic_param(msg, &diffie_hellman,
-				      sizeof(struct hip_diffie_hellman),
+				      sizeof(struct hip_diffie_hellman) - 1,
 				      pubkey);
 
 	HIP_HEXDUMP("Own DH pubkey: ", pubkey, pubkey_len);
