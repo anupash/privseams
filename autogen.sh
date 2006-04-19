1ff1ca1a002678c0f69198f4e8fc1bb6978cc061
Miika Komu <miika@iki.fi>
2006-04-19 15:58:46 +0000

    Readded some files that were mistankenly removed by the previous commit

---
+++ autogen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Generating configure files... may take a while."
+
+autoreconf --install --force && \
+  echo "Preparing was successful if there was no error messages above." && \
+  echo "Now type:" && \
+  echo "  ./configure && make"  && \
+  echo "NOTE: The commands above only build the userspace apps." && \
+  echo "NOTE: You have to build and install the linux kernel separately." && \
+  echo "NOTE: You cannot use HIP without applying the interfamily and beet from the patches directory to your kernel!"
+  echo "NOTE: Some features (e.g. firewall ) require './configure --enable-FEATURE'"
+  echo "NOTE: Run './configure --help' for more information"
+  echo "NOTE: libjip and hipsock need to be compiled separately with make"
