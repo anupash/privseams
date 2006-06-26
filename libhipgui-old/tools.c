f156bed8e8109c9cb94084c4bf75688f8e92f84e
Antti Partanen <aehparta@cc.hut.fi>
2006-06-26 11:44:06 +0000

    Rearrangements to libhipgui. Rearranging and rewriting GUI code. Files renamed/moved/deleted and so on.

--- libhipgui-old/tools.c
+++ libhipgui-old/tools.c
@@ -1,33 +1,37 @@
 /*
-    HIP Agent
-    
+    DNET - Duge's Networking Library
+
     License: GNU/GPL
     Authors: Antti Partanen <aehparta@cc.hut.fi>
 */
 
 /******************************************************************************/
 /* INCLUDES */
+#include "tools.h"
 
-/* STANDARD */
 
-/* THIS */
-#include "x.h"
+/******************************************************************************/
+/* VARIABLES */
+char nickname[32 + 1];
 
 
 /******************************************************************************/
 /* FUNCTIONS */
 
 /******************************************************************************/
-/**
-	This is a template file for new files.
-	
-	@param x x
-	@return value
-*/
-int function(int x)
+/** Get current nickname. */
+char *get_nick(void)
 {
-	/* Return. */
-	return (0);
+	return (nickname);
+}
+/* END OF FUNCTION */
+
+
+/******************************************************************************/
+/** Set current nickname. */
+void set_nick(char *newnick)
+{
+	strncpy(nickname, newnick, 32);
 }
 /* END OF FUNCTION */
 
--- libhipgui-old/tools.h
+++ libhipgui-old/tools.h
@@ -1,22 +1,22 @@
 /*
-    HIP Agent
-    
+    DNET - Duge's Networking Library
+
     License: GNU/GPL
     Authors: Antti Partanen <aehparta@cc.hut.fi>
 */
 
-#ifndef X_H
-#define X_H
+#ifndef TOOLS_H
+#define TOOLS_H
 
 /******************************************************************************/
 /* INCLUDES */
+#include <stdlib.h>
 
 
 /******************************************************************************/
 /* FUNCTION DEFINITIONS */
-
-/* This is a template file for new files. */
-int function(int);
+char *get_nick(void);
+void set_nick(char *);
 
 
 #endif /* END OF HEADER FILE */
