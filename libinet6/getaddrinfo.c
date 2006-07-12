30674a8a3de0c56d1c47fbc4506e0ba711c459c1
Miika Komu <miika@iki.fi>
2006-07-12 19:55:07 +0000

    Potential bug fix to opp. hip. Using nc6 with opp HIP resulted the following error according to Antti:
    nc6: symbol lookup error: ./libopphip/.libs/libopphip.so: undefined 
    symbol: get_local_hits_wrapper
    
    Hopefully works now.

--- libinet6/getaddrinfo.c
+++ libinet6/getaddrinfo.c
@@ -1015,15 +1015,6 @@ gaih_inet_get_name(const char *name, const struct addrinfo *req,
   return 0;
 }
 
-hip_hit_t *get_local_hits_wrapper()
-{
-  struct gaih_addrtuple *at = NULL;
-  struct gaih_addrtuple **pat = &at;
-  
-  get_local_hits(NULL, pat);
-  return (hip_hit_t *)(&at->addr);
-}
-
 static int
 gaih_inet (const char *name, const struct gaih_service *service,
 	   const struct addrinfo *req, struct addrinfo **pai,
