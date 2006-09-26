7e0964352ec0e311a6a753dc718bd200da708663
Antti Partanen <aehparta@cc.hut.fi>
2006-09-26 10:13:30 +0000

    started converting hipconf-tool to functions in libhiptool. executing application doesnt use anymore hardcoded library path.

--- hipd/maintenance.c
+++ hipd/maintenance.c
@@ -378,8 +378,7 @@ int periodic_maintenance()
 			err = hip_count_open_connections();
 			if (err < 1) hipd_set_state(HIPD_STATE_CLOSED);
 		} else {
-			hip_exit(signal);
-			exit(signal);
+			hip_close(SIGINT);
 		}
 		force_exit_counter--;
 	}
