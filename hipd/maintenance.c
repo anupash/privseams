a0771e200c218e4b709f86e2ff54816e2666c7a2
Antti Partanen <aehparta@cc.hut.fi>
2006-09-26 10:15:54 +0000

    fixed signal warning

--- hipd/maintenance.c
+++ hipd/maintenance.c
@@ -378,7 +378,8 @@ int periodic_maintenance()
 			err = hip_count_open_connections();
 			if (err < 1) hipd_set_state(HIPD_STATE_CLOSED);
 		} else {
-			hip_close(SIGINT);
+			hip_exit(SIGINT);
+			exit(SIGINT);
 		}
 		force_exit_counter--;
 	}
