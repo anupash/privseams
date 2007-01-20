a915e5a994b6534c5f2aec4801007501a2d03571
Miika Komu <miika@iki.fi>
2007-01-20 15:16:49 +0000

    Added some debugs

--- hipd/input.c
+++ hipd/input.c
@@ -1454,7 +1454,7 @@ int hip_handle_i2(struct hip_common *i2, struct in6_addr *i2_saddr,
 	if (hip_hadb_hit_is_our(&i2->hitr)) {
 		/* loopback */
 		HIP_IFEL(hip_verify_packet_hmac(i2, &ctx->hip_hmac_out),
-			 -ENOENT, "HMAC validation on i2 failed\n");
+			 -ENOENT, "HMAC loopback validation on i2 failed\n");
 	} else {
 		HIP_IFEL(hip_verify_packet_hmac(i2, &ctx->hip_hmac_in),
 			 -ENOENT, "HMAC validation on i2 failed\n");
