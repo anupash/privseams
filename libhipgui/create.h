a7f635b9ac15ce641db047736f2259ab22d10f09
Antti Partanen <aehparta@cc.hut.fi>
2007-08-01 01:43:35 +0000

    forgot many files :P

---
+++ libhipgui/create.h
@@ -0,0 +1,28 @@
+/*
+ * HIPL GTK GUI
+ *
+ * License: GNU/GPL
+ * Authors: Antti Partanen <aehparta@cc.hut.fi>
+ */
+
+#ifndef _CREATE_H
+#define _CREATE_H
+
+/******************************************************************************/
+/* INCLUDES */
+#include "widgets.h"
+#include "events.h"
+#include "hipgui.h"
+#include "dragndrop.h"
+
+
+/******************************************************************************/
+/* FUNCTION DEFINITIONS */
+int main_create_content(void);
+int create_content_local_edit(void);
+int create_content_msgdlg(void);
+
+
+#endif /* END OF HEADER FILE */
+/******************************************************************************/
+
