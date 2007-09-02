ddb9078a920b2617c2a567f6fb58b2c4de56643e
Miika Komu <miika@iki.fi>
2007-09-02 09:41:28 +0000

    Experimental patch for UPDATE retransmissions

--- hipd/maintenance.c
+++ hipd/maintenance.c
@@ -45,8 +45,8 @@ int hip_handle_retransmission(hip_ha_t *entry, void *current_time)
 	if(*now - HIP_RETRANSMIT_WAIT > entry->hip_msg_retrans.last_transmit){
 		HIP_DEBUG("%d %d %d\n",entry->hip_msg_retrans.count, entry->state, entry->retrans_state);
 		if ((entry->hip_msg_retrans.count > 0) &&
-		    (entry->state != HIP_STATE_ESTABLISHED) &&
-		    (entry->retrans_state == entry->state)) {
+		    (entry->state != HIP_STATE_ESTABLISHED || entry->update_state != 0) &&
+		    (entry->retrans_state == entry->state || entry->retrans_state == entry->update_state)) {
 
 			/* @todo: verify that this works over slow ADSL line */
 			
@@ -78,8 +78,11 @@ int hip_handle_retransmission(hip_ha_t *entry, void *current_time)
 	}
 
  out_err:
-	entry->retrans_state = entry->state;
-		
+	if (entry->state == HIP_STATE_ESTABLISHED)
+		entry->retrans_state = entry->update_state;
+	else
+		entry->retrans_state = entry->state;
+	
 	return err;
 }
 
