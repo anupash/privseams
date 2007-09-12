c93501966a78832139f5e81d18443397f732bc19
Lauri Silvennoinen <ljsilven@tml.hut.fi>
2007-09-12 10:05:13 +0000

    hiprelay.c and hiprelay.h files missing from previous commit.

---
+++ hipd/hiprelay.c
@@ -0,0 +1,204 @@
+/** @file
+ * This file defines a rendezvous extension for the Host Identity Protocol
+ * (HIP).
+ * 
+ * @author  Lauri Silvennoinen
+ * @version 1.0
+ * @date    10.9.2007
+ * @note    Related draft:
+ *          <a href="http://www.ietf.org/internet-drafts/draft-ietf-hip-nat-traversal-02.txt">
+ *          draft-ietf-hip-nat-traversal-02</a>
+ * @note    Distributed under <a href="http://www.gnu.org/licenses/gpl.txt">GNU/GPL</a>.
+ */ 
+
+
+#include "hiprelay.h"
+
+/** The hashtable storing the relay records. */
+static LHASH *hiprelay_ht = NULL;
+
+LHASH *hip_relht_init()
+{
+     return lh_new(LHASH_HASH_FN(hip_relht_hash), LHASH_COMP_FN(hip_relht_compare));
+}
+
+void hip_relht_uninit()
+{
+     if(hiprelay_ht == NULL)
+	  return;
+
+     lh_doall(hiprelay_ht, LHASH_DOALL_FN(hip_relht_rec_free));
+     lh_free(hiprelay_ht);
+}
+
+unsigned long hip_relht_hash(const hip_relrec_t *rec)
+{
+     if(rec == NULL)
+	  return 0;
+
+     return hip_hash_hit(&(rec->hit_r));
+}
+
+int hip_relht_compare(const hip_relrec_t *rec1, const hip_relrec_t *rec2)
+{
+     if(rec1 == NULL || rec2 == NULL)
+	  return 1;
+
+     return hip_match_hit(&(rec1->hit_r), &(rec2->hit_r));
+}
+
+void hip_relht_put(hip_relrec_t *rec)
+{
+     if(hiprelay_ht == NULL || rec == NULL)
+	  return;
+
+     /* lh_insert returns always NULL, we cannot return nothing from this function. */
+     lh_insert(hiprelay_ht, rec);
+}
+
+hip_relrec_t *hip_relht_get(hip_relrec_t *rec)
+{
+     if(hiprelay_ht == NULL || rec == NULL)
+	  return NULL;
+
+     return (hip_relrec_t *)lh_retrieve(hiprelay_ht, rec);
+}
+
+void hip_relht_rec_free(hip_relrec_t *rec)
+{
+     if(hiprelay_ht == NULL || rec == NULL)
+	  return;
+
+     /* Check if such element exist, and delete the pointer from the hashtable. */
+     hip_relrec_t *deleted_rec = lh_delete(hiprelay_ht, rec);
+
+     /* Free the memory allocated for the element. */
+     if(deleted_rec != NULL)
+     {
+	  memset(deleted_rec, '\0', sizeof(*deleted_rec));
+	  free(deleted_rec);
+     }
+}
+
+void hip_relht_free_expired(hip_relrec_t *rec)
+{
+     if(rec == NULL)
+	  return;
+
+     if(time(NULL) - rec->last_contact > HIP_RELREC_LIFETIME)
+     {
+	  HIP_INFO("Relay record expired, deleting.\n");
+	  lh_delete(hiprelay_ht, rec);
+	  memset(rec, '\0', sizeof(*rec));
+	  free(rec);
+     }
+}
+
+unsigned long hip_relht_size()
+{
+     if(hiprelay_ht == NULL)
+	  return 0;
+
+     return hiprelay_ht->num_items;
+}
+
+void hip_relht_maintenance()
+{
+     if(hiprelay_ht == NULL)
+	  return;
+     
+     unsigned int tmp = hiprelay_ht->down_load;
+     hiprelay_ht->down_load = 0;
+     lh_doall(hiprelay_ht, LHASH_DOALL_FN(hip_relht_free_expired));
+     hiprelay_ht->down_load = tmp;
+}
+
+hip_relrec_t *hip_relrec_alloc(struct in6_addr *hit_r, struct in6_addr *ip_r,
+				 hip_relrec_mode_t mode)
+{
+     if(hit_r == NULL || ip_r == NULL)
+	  return NULL;
+
+     hip_relrec_t *rec = (hip_relrec_t*) malloc(sizeof(hip_relrec_t));
+     
+     if(rec == NULL)
+     {
+	  HIP_ERROR("Error allocating memory for HIP relay record.\n");
+	  return NULL;
+     }
+
+     rec->flags = 0;
+     hip_relrec_set_mode(rec, mode);
+     memcpy(&(rec->hit_r), hit_r, sizeof(*hit_r));
+     memcpy(&(rec->ip_r), ip_r, sizeof(*ip_r));
+     rec->lifetime = HIP_RELREC_LIFETIME;
+     rec->last_contact = time(NULL);
+     
+     return rec;
+}
+
+void hip_relrec_set_mode(hip_relrec_t *rec, hip_relrec_mode_t mode)
+{
+     if(rec == NULL)
+	  return;
+     
+     switch (mode){
+     case HIP_REL_ON:
+	  rec->flags |= 0x10;
+	  break;
+     case HIP_REL_OFF:
+	  rec->flags &= ~0x10;
+	  break;
+     case HIP_REL_NONE_TO_NONE:
+	  rec->flags &= 0xf0;
+	  break;
+     case HIP_REL_NONE_TO_UDP:
+	  rec->flags &= 0xf0;
+	  rec->flags |= 0x01;
+	  break;
+     case HIP_REL_UDP_TO_NONE:
+	  rec->flags &= 0xf0;
+	  rec->flags |= 0x04;
+	  break;
+     case HIP_REL_UDP_TO_UDP:
+	  rec->flags &= 0xf0;
+	  rec->flags |= 0x05;
+	  break;
+     case HIP_REL_TCP_TO_TCP:
+	  rec->flags &= 0xf0;
+	  rec->flags |= 0x0a;
+	  break;
+     }
+}
+
+void hip_relrec_info(hip_relrec_t *rec)
+{
+     if(rec == NULL)
+	  return;
+     
+     char status[1024];
+     char *cursor = status;
+     cursor += sprintf(cursor, "Relay record info:\n");
+     cursor += sprintf(cursor, " HIP Relay status: ");
+     cursor += sprintf(cursor, (rec->flags & 0x10) ? "ON\n" : "OFF\n");
+     cursor += sprintf(cursor, " I -> Relay: ");
+     cursor += sprintf(cursor, (rec->flags & 0x04) ? "UDP\n" :
+		       (rec->flags & 0x08) ? "TCP\n" : "None\n");
+     cursor += sprintf(cursor, " Relay -> R: ");
+     cursor += sprintf(cursor, (rec->flags & 0x01) ? "UDP\n" :
+		       (rec->flags & 0x02) ? "TCP\n" : "None\n");
+     cursor += sprintf(cursor, " Record lifetime: %lu seconds\n", rec->lifetime);
+     cursor += sprintf(cursor, " Last contact: %lu seconds ago\n", time(NULL) - rec->last_contact);
+     cursor += sprintf(cursor, " HIT of R: %04x:%04x:%04x:%04x:%04x:%04x:%04x:%04x\n",
+		       rec->hit_r.s6_addr16[0], rec->hit_r.s6_addr16[1],
+		       rec->hit_r.s6_addr16[2], rec->hit_r.s6_addr16[3],
+		       rec->hit_r.s6_addr16[4], rec->hit_r.s6_addr16[5],
+		       rec->hit_r.s6_addr16[6], rec->hit_r.s6_addr16[7]);
+     cursor += sprintf(cursor, " IP of R:  %04x:%04x:%04x:%04x:%04x:%04x:%04x:%04x\n",
+		       rec->ip_r.s6_addr16[0], rec->ip_r.s6_addr16[1],
+		       rec->ip_r.s6_addr16[2], rec->ip_r.s6_addr16[3],
+		       rec->ip_r.s6_addr16[4], rec->ip_r.s6_addr16[5],
+		       rec->ip_r.s6_addr16[6], rec->ip_r.s6_addr16[7]);
+
+     HIP_INFO("\n%s", status);
+}
---
+++ hipd/hiprelay.h
@@ -0,0 +1,214 @@
+/** @file
+ * A header file for hip_relay.c.
+ * 
+ * @author  Lauri Silvennoinen
+ * @version 1.0
+ * @date    10.9.2007
+ * @note    Related draft:
+ *          <a href="http://www.ietf.org/internet-drafts/draft-ietf-hip-nat-traversal-02.txt">
+ *          draft-ietf-hip-nat-traversal-02</a>
+ * @note    Distributed under <a href="http://www.gnu.org/licenses/gpl.txt">GNU/GPL</a>.
+ */
+#ifndef HIP_HIPRELAY_H
+#define HIP_HIPRELAY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h> /* For timing. */
+#include <openssl/lhash.h> /* For LHASH. */
+#include <netinet/in.h> /* For IPv6 addresses etc. */
+#include "hashtable.h" /* For hip hashtable commons. */
+#include "misc.h" /* For hip_hash_hit and hip_match_hit. */
+
+/*
+#include "hadb.h"
+#include "hashtable.h"
+#include "misc.h"
+#include "builder.h"
+#include "output.h"
+#include "state.h"
+*/
+
+/** Default relay record life time in seconds. After this time, the record is
+ *  deleted if it has been idle. */
+#define HIP_RELREC_LIFETIME 600
+
+/** HIP Relay record. These records are stored in the HIP Relay hashtable. */
+typedef struct{
+     /** Flags indicating the status of this record.
+      *  <pre>
+      *    bitmap: 0000 0000
+      *            |||| |||+ RVS -> R UDP     0x01
+      *            |||| ||+- RVS -> R TCP     0x02
+      *            |||| |+-- I -> RVS UDP     0x04
+      *            |||| +--- I -> RVS TCP     0x08
+      *            |||+----- HIP relay status 0x10
+      *            +++------ Not used
+      *  </pre>
+      */
+     uint8_t flags;
+     /** The lifetime of this record, seconds. */
+     time_t lifetime;
+     /** Time when this record was last used, seconds since epoch. */
+     time_t last_contact;
+     /** HIT of Responder (Relay Client) */
+     struct in6_addr hit_r;
+     /** IP address of Responder (Relay Client) */
+     struct in6_addr ip_r;
+}hip_relrec_t;
+
+/** Relay record modes used to flag a relay record. */
+typedef enum{HIP_REL_NONE_TO_NONE, HIP_REL_NONE_TO_UDP, HIP_REL_UDP_TO_NONE,
+		  HIP_REL_UDP_TO_UDP, HIP_REL_TCP_TO_TCP, HIP_REL_OFF,
+		  HIP_REL_ON}hip_relrec_mode_t;
+
+/**
+ * Initializes the HIP relay hashtable. Allocates memory for a new hashtable.
+ *
+ * @return a pointer to a new hashtable, NULL if failed to init.
+ */ 
+LHASH *hip_relht_init();
+
+/** 
+ * Uninitializes the HIP relay hashtable. Frees the memory allocated for the hashtable
+ * and for the relay records. Thus after calling this function, all memory allocated
+ * from heap related to the relay records is freed.
+ */
+void hip_relht_uninit();
+
+/**
+ * The hash function of the hashtable. Calculates a has from parameter relay record
+ * HIT.
+ * 
+ * @param rec a pointer to a relay record.
+ * @return    the calculated hash.
+ */
+unsigned long hip_relht_hash(const hip_relrec_t *rec);
+
+/**
+ * The compare function of the hashtable. Compares the hash values calculated from
+ * parameter @c rec1 and @c rec2.
+ * 
+ * @param rec1 a pointer to a relay record.
+ * @param rec2 a pointer to a relay record.
+ * @return     0 if keys are equal, non-zero otherwise.
+ */
+int hip_relht_compare(const hip_relrec_t *rec1, const hip_relrec_t *rec2);
+
+/**
+ * Puts a relay record in the hashtable. Puts the relay record pointed to by @c rec
+ * into the hashtable. If there already is an entry with the same key, the old value
+ * is replaced. Note that we store pointers here, the data are not copied. There
+ * should be no need to put a relay record more than once into the hashtable. If
+ * the fields of an individual relay record need to be changed, just retrieve the
+ * record with @c hip_relht_get() and alter the fields of it, but do not re-put it.
+ *
+ * @param rec a pointer to a relay record to be inserted into the hashtable.
+ * @note      <b style="color: #f00;">Do not put records allocated from stack into the
+ *            hashtable.</b> Instead put only records created with hip_relrec_alloc().
+ */
+void hip_relht_put(hip_relrec_t *rec);
+
+/**
+ * Retrieves a relay record from the hashtable. The parameter record @c rec needs
+ * only to have field @c hit_r populated.
+ *
+ * @param rec a pointer to a relay record.
+ * @return    a pointer to a fully populated relay record if found, NULL otherwise.
+ */
+hip_relrec_t *hip_relht_get(hip_relrec_t *rec);
+
+/**
+ * Deletes a single entry from the relay record hashtable and frees the memory allocated
+ * for the element. The deletion is based on the hash calculated from the relay fecord
+ * @c hit_r field, and therefore the parameter record does not need to be fully populated.
+ * The parameter relay record is itself left untouched, it is only used as an search key.
+ *
+ * @param rec a pointer to a relay record. 
+ */
+void hip_relht_rec_free(hip_relrec_t *rec);
+
+/**
+ * Deletes a single entry from the relay record hashtable and frees the memory
+ * allocated for the record, if the record has expired. The relay record is deleted if
+ * it has been last contacted more than @c HIP_RELREC_LIFETIME seconds ago.
+ */
+void hip_relht_free_expired(hip_relrec_t *rec);
+
+/**
+ * Returns the number of relay records in the hashtable.
+ * 
+ * @return  number of relay records in the hashtable.
+ */
+unsigned long hip_relht_size();
+
+/**
+ * Periodic maintenance function of the hip relay. This function should be called once in
+ * every maintenance cycle of the hip daemon. It clears the expired relay records by
+ * calling @c hip_relht_free_expired() for every element in the hashtable.
+ */
+void hip_relht_maintenance();
+
+/**
+ * Allocates a new relay record.
+ * 
+ * @param hit_r a pointer to Responder (relay client) HIT.
+ * @param ip_r  a pointer to Responder (relay client) IP address.
+ * @param mode  the mode of this record (ON/OFF or encapsulation mode). Usually
+ *              @c HIP_REL_ON.
+ * @return      a pointer to a new relay record, or NULL if failed to allocate.
+ * @note        All records to be put in the hashtable should be created with this
+ *              function.
+ * @note        After calling this function, you should set the appropriate encapsulation
+ *              mode for the @c record with hip_relrec_set_mode().
+ */
+hip_relrec_t *hip_relrec_alloc(struct in6_addr *hit_r, struct in6_addr *ip_r,
+			       hip_relrec_mode_t mode);
+
+/**
+ * Sets the mode of a relay record. This function sets the @c flags field of a
+ * relay record. In the encapsulation modes, the first part in the names is from
+ * Initiator to relay and the second part is from relay to Responder (relay client).
+ * So HIP_REL_NONE_TO_UDP means <code>I--(raw HIP)--Relay--(UDP)--R</code>.
+ * 
+ * @param rec  a pointer to a relay record. 
+ * @param mode the mode to be set for the parameter record. One of the following:
+ *             <ul>
+ *             <li>HIP_REL_NONE_TO_NONE</li>
+ *             <li>HIP_REL_NONE_TO_UDP</li>
+ *             <li>HIP_REL_UDP_TO_NONE</li>
+ *             <li>HIP_REL_UDP_TO_UDP</li>
+ *             <li>HIP_REL_TCP_TO_TCP</li>
+ *             <li>HIP_REL_OFF</li>
+ *             <li>HIP_REL_ON</li>
+ *             </ul>
+ * @see        hip_relrec_t for a bitmap.
+ */
+void hip_relrec_set_mode(hip_relrec_t *rec, hip_relrec_mode_t mode);
+
+/**
+ * Sets the lifetime of a relay record. 
+ * 
+ * @param rec  a pointer to a relay record. 
+ * @param mode the lifetime in seconds. 
+ */
+void hip_relrec_set_lifetime(hip_relrec_t *rec, time_t secs);
+
+/**
+ * Prints info of the parameter relay record using @c HIP_INFO() macro.
+ * 
+ * @param rec a pointer to a relay record.
+ */
+void hip_relrec_info(hip_relrec_t *rec);
+
+/** A callback wrapper of the prototype required by @c lh_new(). */
+static IMPLEMENT_LHASH_HASH_FN(hip_relht_hash, const hip_relrec_t *)
+/** A callback wrapper of the prototype required by @c lh_new(). */
+static IMPLEMENT_LHASH_COMP_FN(hip_relht_compare, const hip_relrec_t *)
+/** A callback wrapper of the prototype required by @c lh_doall(). */
+static IMPLEMENT_LHASH_DOALL_FN(hip_relht_rec_free, hip_relrec_t *)
+/** A callback wrapper of the prototype required by @c lh_doall(). */
+static IMPLEMENT_LHASH_DOALL_FN(hip_relht_free_expired, hip_relrec_t *)
+
+#endif /* HIP_HIPRELAY_H */
