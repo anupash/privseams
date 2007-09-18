943bb1038cdaf85fcf9a7ae8b7cf215ecca0a65f
Alberto Garcia <alberto.garcia@hiit.fi>
2007-09-18 10:33:28 +0000

    Database oppipdb, to store non-HIP capable hosts in the opp mode, implemented as a hash table.

--- hipd/init.c
+++ hipd/init.c
@@ -177,8 +177,8 @@ int hipd_init(int flush_ipsec, int killold)
 	signal(SIGTERM, hip_close);
 	signal(SIGCHLD, hip_sig_chld);
  
-	HIP_IFEL(hip_ipdb_clear(), -1,
-	         "Cannot clear opportunistic mode IP database for non HIP capable hosts!\n");
+	HIP_IFEL(hip_init_oppip_db(), -1,
+	         "Cannot initialize opportunistic mode IP database for non HIP capable hosts!\n");
 
 	HIP_IFEL((hip_init_cipher() < 0), 1, "Unable to init ciphers.\n");
 
