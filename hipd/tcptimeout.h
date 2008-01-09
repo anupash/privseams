3fd67807db872272ee1fad7760444966e75a0c12
Tao Wan <twan@cc.hut.fi>
2008-01-09 17:16:03 +0000

    hipconf tcp_timeout parameters setting sent to hipd is done

---
+++ hipd/tcptimeout.h
@@ -0,0 +1,21 @@
+/** @file
+ * This file defines TCP timeout parameters setting for the Host Identity
+ * Protocol (HIP) in order to overcome the application time out when handover taking 
+ * long time.
+ *      
+ * @author  Tao Wan  <twan_cc.hut.fi>
+ * @note    Distributed under <a href="http://www.gnu.org/licenses/gpl.txt">GNU/GPL</a>.
+ * */
+
+
+#ifndef HIP_TCPTIMEOUT
+#define HIP_TCPTIMEOUT
+
+
+
+
+
+
+#endif /* HIP_TCPTIMEOUT */
+
+
