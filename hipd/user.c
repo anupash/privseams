4f24720a5b4515ba409a025c3247d92b6f32d373
Tao Wan <twan@cc.hut.fi>
2008-03-03 16:01:20 +0000

    solved SO_HIP_TRIGGER_BEX part compling problem in hipd/user.c

--- hipd/user.c
+++ hipd/user.c
@@ -556,7 +556,7 @@ int hip_handle_user_msg(struct hip_common *msg, const struct sockaddr_in6 *src)
 		break;
 #endif
 	case SO_HIP_TRIGGER_BEX:
-		dst_hit = hip_param_contents(msg, HIP_PARAM_HIT);
+		dst_hit = hip_get_param_contents(msg, HIP_PARAM_HIT);
 		HIP_IFEL(hip_add_peer_map(msg), -1, "trigger bex\n");
 		/* Fetch the hadb entry just created. */
 		HIP_IFEL(!(entry = hip_hadb_try_to_find_by_peer_hit(dst_hit)),
