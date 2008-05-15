aea245e9d5a43cb0187b07e861e785d63da0f1fd
Thomas Jansen <mithi@mithi.net>
2008-05-15 15:26:34 +0000

    Save commit.
    changed nonce format from SHA1 to HMAC/SHA1
    started signature checking

--- firewall/pisa.c
+++ firewall/pisa.c
@@ -11,13 +11,23 @@
 #include <string.h>
 
 #define PISA_RANDOM_LEN 16
+#define PISA_PUZZLE_SEED 0xDEADC0DE
+#define PISA_NONCE_LEN (4 + HIP_AH_SHA_LEN)
 
-union pisa_puzzle_hash {
-	u8 sha[HIP_AH_SHA_LEN];
-	struct {
-		uint64_t random;
-		u8 opaque[6];
-	} pz;
+/*#define PISA_TEST_MULTIPLE_PARAMETERS*/
+/*#define PISA_INTRODUCE_ERROR_NONCE*/
+/*#define PISA_INTRODUCE_ERROR_PUZZLE_OPAQUE*/
+/*#define PISA_INTRODUCE_ERROR_PUZZLE_RANDOM*/
+
+struct pisa_puzzle_hash {
+	u8 data[4];
+	union {
+		u8 raw[HIP_AH_SHA_LEN];
+		struct {
+			uint64_t random;
+			u8 opaque[6];
+		} pz;
+	} u;
 };
 
 static char pisa_random_data[2][PISA_RANDOM_LEN];
@@ -32,30 +42,38 @@ static void pisa_generate_random()
 }
 
 /**
- * Compute a PISA nonce and place it in preallocated buffer. A PISA nonce is a
- * SHA1 digest of the concatenation of initiator HIT, responder HIT and the
- * current random number.
+ * Appends HMAC/SHA1 to a block of data.
  *
  * @param a first HIT
  * @param b second HIT
  * @param rnd which random number to use, either 0 or 1
- * @param digest pointer to buffer for the nonce, needs HIP_AH_SHA_LEN bytes
+ * @param spi SPI of that will be signed
+ * @param data pointer to buffer for data and the HMAC
  */
-static void pisa_create_nonce_hash(struct in6_addr *a, struct in6_addr *b, 
-                                   int rnd, u8 *digest)
+static void pisa_append_hmac(struct in6_addr *a, struct in6_addr *b, 
+                                   int rnd, u32 spi, void *data, int data_len)
 {
-	u8 raw[32 + PISA_RANDOM_LEN];
+	u8 key[36 + PISA_RANDOM_LEN];
+	int len = HIP_AH_SHA_LEN;
 
-	if (!(digest && a && b) || (rnd != 0 && rnd != 1))  {
+	if (!(data && a && b) || (rnd != 0 && rnd != 1) || data_len == 0)  {
 		HIP_ERROR("failed pisa_create_nonce_hash sanity check.\n");
 		return;
 	}
 
-	ipv6_addr_copy((struct in6_addr *)raw, a);
-	ipv6_addr_copy((struct in6_addr *)(raw+16), b);
-	memcpy(raw+32, &pisa_random_data[rnd][0], PISA_RANDOM_LEN);
+	/* The key for HMAC/SHA1 consists of:
+	 *    4 bytes SPI
+	 *   16 bytes HIT1
+	 *   16 bytes HIT2
+	 *            pisa_random_data
+	 */
 
-	hip_build_digest(HIP_DIGEST_SHA1, raw, 32 + PISA_RANDOM_LEN, digest);
+	memcpy(key, &spi, sizeof(u32));
+	ipv6_addr_copy((struct in6_addr *)(key+ 4), a);
+	ipv6_addr_copy((struct in6_addr *)(key+20), b);
+	memcpy(key+36, &pisa_random_data[rnd][0], PISA_RANDOM_LEN);
+
+	HMAC(EVP_sha1(), key, 36 + PISA_RANDOM_LEN, data, data_len, data + data_len, &len);
 }
 
 /**
@@ -66,16 +84,34 @@ static void pisa_create_nonce_hash(struct in6_addr *a, struct in6_addr *b,
  */
 static int pisa_insert_nonce(struct midauth_packet *p)
 {
-	u8 sha[HIP_AH_SHA_LEN];
+	u8 nonce[PISA_NONCE_LEN];
+	struct hip_esp_info *esp_info;
+	u32 spi;
 
-	/* @todo FIXME BEGIN --- for testing purposes only */
-/*	char *nonce1 = "asdfasdfasdf", *nonce2 = "01234567890123456789";
-	midauth_add_echo_request_m(p, nonce1, strlen(nonce1));
-	midauth_add_echo_request_m(p, nonce2, strlen(nonce2));*/
-	/* @todo FIXME END --- for testing purposes only */
+	esp_info = hip_get_param(p->hip, HIP_PARAM_ESP_INFO);
+	spi = esp_info->new_spi;
 
-	pisa_create_nonce_hash(&p->hip->hits, &p->hip->hitr, 1, sha);
-	return midauth_add_echo_request_m(p, sha, HIP_AH_SHA_LEN);
+#ifdef PISA_TEST_MULTIPLE_PARAMETERS
+	{
+		char *nonce1 = "asdf", *nonce2 = "012345678901234567890123";
+		midauth_add_echo_request_m(p, nonce1, strlen(nonce1));
+		midauth_add_echo_request_m(p, nonce2, strlen(nonce2));
+	}
+#endif
+	/* The nonce looks like this:
+	 *    4 bytes SPI
+	 *   20 bytes HMAC of everything before
+	 * As we only use the data in the firewall, byteorder is not an issue.
+	 */
+
+	memcpy(nonce, &spi, sizeof(u32));
+	pisa_append_hmac(&p->hip->hits, &p->hip->hitr, 1, spi, nonce, 4);
+
+#ifdef PISA_INTRODUCE_ERROR_NONCE
+	nonce[0]++;
+#endif
+
+	return midauth_add_echo_request_m(p, nonce, PISA_NONCE_LEN);
 }
 
 /**
@@ -86,18 +122,29 @@ static int pisa_insert_nonce(struct midauth_packet *p)
  */
 static int pisa_insert_puzzle(struct midauth_packet *p)
 {
-	union pisa_puzzle_hash hash;
+	struct pisa_puzzle_hash hash;
+	int seed = PISA_PUZZLE_SEED;
+
+	memcpy(&hash, &seed, 4);
 
 	/* here we switch order of initiator and receiver to obtain an other
 	 * SHA1 hash */
-	pisa_create_nonce_hash(&p->hip->hitr, &p->hip->hits, 1, hash.sha);
+	pisa_append_hmac(&p->hip->hitr, &p->hip->hits, 1, 0, &hash, 4);
 
-	/* @todo FIXME BEGIN --- for testing purposes only */
-/*	midauth_add_puzzle_m(p, 3, 4, "puzzle", 0xABCDABCDABCDABCDLL);
-	midauth_add_puzzle_m(p, 3, 4, "abcdef", 0x0123456789ABCDEFLL);*/
-	/* @todo FIXME END --- for testing purposes only */
+#ifdef PISA_TEST_MULTIPLE_PARAMETERS
+	midauth_add_puzzle_m(p, 3, 4, "puzzle", 0xABCDABCDABCDABCDLL);
+	midauth_add_puzzle_m(p, 3, 4, "abcdef", 0x0123456789ABCDEFLL);
+#endif
 
-	return midauth_add_puzzle_m(p, 3, 4, hash.pz.opaque, hash.pz.random);
+#ifdef PISA_INTRODUCE_ERROR_PUZZLE_OPAQUE
+	hash.u.pz.opaque[0]++;
+#endif
+
+#ifdef PISA_INTRODUCE_ERROR_PUZZLE_RANDOM
+	hash.u.pz.random++;
+#endif
+
+	return midauth_add_puzzle_m(p, 3, 4, hash.u.pz.opaque, hash.u.pz.random);
 }
 
 /**
@@ -112,12 +159,9 @@ static int pisa_insert_puzzle(struct midauth_packet *p)
 static int pisa_check_nonce(struct midauth_packet *p)
 {
 	struct hip_tlv_common *nonce;
-	u8 sha[2][HIP_AH_SHA_LEN], *nonce_data;
+	u8 valid[PISA_NONCE_LEN], *nonce_data;
+	u32 spi;
 	int nonce_len;
-
-	/* get the two values we will accept */
-	pisa_create_nonce_hash(&p->hip->hitr, &p->hip->hits, 0, &sha[0][0]);
-	pisa_create_nonce_hash(&p->hip->hitr, &p->hip->hits, 1, &sha[1][0]);
 
 	nonce = hip_get_param(p->hip, HIP_PARAM_ECHO_RESPONSE_M);
 
@@ -129,13 +173,19 @@ static int pisa_check_nonce(struct midauth_packet *p)
 		nonce_len = hip_get_param_contents_len(nonce);
 		nonce_data = (u8 *)hip_get_param_contents_direct(nonce);
 
-		/* if the payload has the size of a SHA1 digest ... */
-		if (nonce_len == HIP_AH_SHA_LEN) {
+		/* if the payload has the size of a nonce ... */
+		if (nonce_len == PISA_NONCE_LEN) {
+			memcpy(valid, nonce_data, 4);
+			spi = *((u32 *)nonce_data);
+
 			/* ... first check the current random value ... */
-			if (!memcmp(&sha[1][0], nonce_data, HIP_AH_SHA_LEN))
+			pisa_append_hmac(&p->hip->hitr, &p->hip->hits, 1, spi, valid, 4);
+			if (!memcmp(valid, nonce_data, PISA_NONCE_LEN))
 				return 0;
+
 			/* ... and if that failed the old random value */
-			if (!memcmp(&sha[0][0], nonce_data, HIP_AH_SHA_LEN))
+			pisa_append_hmac(&p->hip->hitr, &p->hip->hits, 0, spi, valid, 4);
+			if (!memcmp(valid, nonce_data, PISA_NONCE_LEN))
 				return 0;
 		}
 
@@ -154,10 +204,14 @@ static int pisa_check_nonce(struct midauth_packet *p)
 static int pisa_check_solution(struct midauth_packet *p)
 {
 	struct hip_solution_m *solution;
-	union pisa_puzzle_hash hash[2];
+	struct pisa_puzzle_hash hash[2];
+	int seed = PISA_PUZZLE_SEED;
 
-	pisa_create_nonce_hash(&p->hip->hits, &p->hip->hitr, 0, hash[0].sha);
-	pisa_create_nonce_hash(&p->hip->hits, &p->hip->hitr, 1, hash[1].sha);
+	memcpy(&hash[0], &seed, 4);
+	memcpy(&hash[1], &seed, 4);
+
+	pisa_append_hmac(&p->hip->hits, &p->hip->hitr, 0, 0, &hash[0], 4);
+	pisa_append_hmac(&p->hip->hits, &p->hip->hitr, 1, 0, &hash[1], 4);
 
 	solution = (struct hip_solution_m *)
 	           hip_get_param(p->hip, HIP_PARAM_SOLUTION_M);
@@ -167,10 +221,10 @@ static int pisa_check_solution(struct midauth_packet *p)
 		if (hip_get_param_type(solution) != HIP_PARAM_SOLUTION_M)
 			break;
 
-		if ((!memcmp(solution->opaque, hash[1].pz.opaque, 6)
-		      && solution->I == hash[1].pz.random) ||
-		    (!memcmp(solution->opaque, hash[0].pz.opaque, 6)
-		      && solution->I == hash[0].pz.random)) {
+		if ((!memcmp(solution->opaque, hash[1].u.pz.opaque, 6)
+		      && solution->I == hash[1].u.pz.random) ||
+		    (!memcmp(solution->opaque, hash[0].u.pz.opaque, 6)
+		      && solution->I == hash[0].u.pz.random)) {
 			if (midauth_verify_solution_m(p->hip, solution) == 0)
 				return 0;
 		}
@@ -180,6 +234,29 @@ static int pisa_check_solution(struct midauth_packet *p)
 	}
 
 	return -1;
+}
+
+/**
+ * Check the signature of the packet.
+ *
+ * @param p packet with the signature to check
+ * @return success (0) or failure
+ */
+static int pisa_check_signature(struct midauth_packet *p)
+{
+	int err = 0;
+	struct hip_host_id *host_id;
+	int (*verify_signature)(struct hip_host_id *, struct hip_common *);
+
+	host_id = hip_get_param(p->hip, HIP_PARAM_HOST_ID);
+	HIP_IFEL(host_id, -1, "No HOST_ID found to check signature.\n");
+
+	verify_signature = (hip_get_host_id_algo(host_id) == HIP_HI_RSA ? hip_rsa_verify : hip_dsa_verify);
+
+	err = verify_signature(host_id, p->hip);
+
+out_err:
+	return err;
 }
 
 /**
@@ -230,12 +307,13 @@ static int pisa_handler_i2(struct midauth_packet *p)
  */
 static int pisa_handler_r2(struct midauth_packet *p)
 {
-	int verdict = NF_DROP, nonce = 0, solution = 0;
+	int verdict = NF_DROP, nonce = 0, solution = 0, sig;
 
 	nonce = pisa_check_nonce(p);
 	solution = pisa_check_solution(p);
+	sig = pisa_check_signature(p);
 
-	if (nonce != 0 || solution != 0) {
+	if (nonce != 0 || solution != 0 || sig != 0) {
 		/* disallow further communication if either nonce or solution
 		 * were not correct */
 		pisa_reject_connection(p);
