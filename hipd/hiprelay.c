61a3f1ad5d89c82d2931fa0165d33765da7500dc
Miika Komu <miika@iki.fi>
2008-05-23 05:26:41 +0000

    Compilation fixes. Reverted hiprelay.c and header file to what is in userspace

--- hipd/hiprelay.c
+++ hipd/hiprelay.c
@@ -39,13 +39,9 @@ hip_relay_status_t relay_enabled = HIP_RELAY_OFF;
 hip_relay_wl_status_t whitelist_enabled = HIP_RELAY_WL_ON;
 
 /** A callback wrapper of the prototype required by @c lh_new(). */
-static IMPLEMENT_LHASH_HASH_FN(hip_relwl_hash, const hip_hit_t *)
+static IMPLEMENT_LHASH_HASH_FN(hip_relht_hash, const hip_relrec_t *)
 /** A callback wrapper of the prototype required by @c lh_new(). */
-static IMPLEMENT_LHASH_COMP_FN(hip_relwl_compare, const hip_hit_t *)
-/** A callback wrapper of the prototype required by @c lh_doall(). */
-static IMPLEMENT_LHASH_DOALL_FN(hip_relwl_hit_free, hip_hit_t *)
-
-#if 0
+static IMPLEMENT_LHASH_COMP_FN(hip_relht_compare, const hip_relrec_t *)
 /** A callback wrapper of the prototype required by @c lh_doall(). */
 static IMPLEMENT_LHASH_DOALL_FN(hip_relht_rec_free, hip_relrec_t *)
 /** A callback wrapper of the prototype required by @c lh_doall(). */
@@ -53,18 +49,13 @@ static IMPLEMENT_LHASH_DOALL_FN(hip_relht_rec_free_expired, hip_relrec_t *)
 /** A callback wrapper of the prototype required by @c lh_doall_arg(). */
 static IMPLEMENT_LHASH_DOALL_ARG_FN(hip_relht_rec_free_type, hip_relrec_t *,
 				    const hip_relrec_type_t *)
-#endif
 
-/** 
- * A dummy boolean to indicate the machine has relay capabilities.
- * This is only here for testing and development purposes. It allows the same
- * code to be used at the relay and at endhosts without C precompiler #ifdefs
- */
-int we_are_relay = 0;
-
-/** The hashtable storing the relay records. */
-//static LHASH *hiprelay_ht = NULL;
- LHASH *hiprelay_ht = NULL;
+/** A callback wrapper of the prototype required by @c lh_new(). */
+static IMPLEMENT_LHASH_HASH_FN(hip_relwl_hash, const hip_hit_t *)
+/** A callback wrapper of the prototype required by @c lh_new(). */
+static IMPLEMENT_LHASH_COMP_FN(hip_relwl_compare, const hip_hit_t *)
+/** A callback wrapper of the prototype required by @c lh_doall(). */
+static IMPLEMENT_LHASH_DOALL_FN(hip_relwl_hit_free, hip_hit_t *)
 
 hip_relay_status_t hip_relay_get_status()
 {
@@ -597,208 +588,9 @@ int hip_relay_rvs(const hip_common_t *i1, const in6_addr_t *i1_saddr,
 	return err;
 }
 
-/**
- * 
- * this relay full function is to use to relay package from i or r.
- * 
- * 
- * 
- * */
-
-int hip_relay_full(const hip_common_t *i1, const in6_addr_t *i1_saddr,
-		  const in6_addr_t *i1_daddr, hip_relrec_t *rec,
-		  const hip_portpair_t *i1_info,
-		  const uint8_t type_hdr)
-{
-     struct hip_common *i1_to_be_relayed = NULL;
-     struct hip_tlv_common *current_param = NULL;
-     int err = 0, from_added = 0;
-     hip_tlv_type_t param_type = 0;
-     /* A function pointer to either hip_build_param_from() or
-	hip_build_param_relay_from(). */
-     int (*builder_function) (struct hip_common *msg,
-			      const struct in6_addr *addr,
-			      const in_port_t port);
-
-     HIP_DEBUG("hip_relay_full() invoked.\n");
-     HIP_DEBUG_IN6ADDR("hip_relay_full():  source address", i1_saddr);
-     HIP_DEBUG_IN6ADDR("hip_relay_full():  destination address", i1_daddr);
-     HIP_DEBUG_HIT("hip_relay_full: Relay record hit",
-		   &rec->hit_r);
-     HIP_DEBUG("Relay record port: %d.\n", rec->udp_port_r);
-     HIP_DEBUG("I1 source port: %u, destination port: %u\n",
-	       i1_info->src_port, i1_info->dst_port);
-		
-     /* If the incoming I1 packet was destined to port 50500, we know that
-	there is a NAT between (I->NAT->RVS->R). */
-     // if(i1_info->dst_port == HIP_NAT_UDP_PORT) {
-     builder_function = hip_build_param_relay_from;
-     param_type = HIP_PARAM_RELAY_FROM;
-	  /*	}
-	else {
-	  builder_function = hip_build_param_from;
-	  param_type = HIP_PARAM_FROM;
-	  }*/
-
-     HIP_IFEL(!(i1_to_be_relayed = hip_msg_alloc()), -ENOMEM,
-	      "No memory to copy original I1\n");	
-
-     /* I1 packet forwarding is achieved by rewriting the source and
-	destination IP addresses. */
-     hip_build_network_hdr(i1_to_be_relayed, type_hdr, 0,
-			   &(i1->hits), &(i1->hitr));
-
-     /* Adding FROM (RELAY_FROM) parameter. Loop through all the parameters in
-	the received I1 packet, and insert a new FROM (RELAY_FROM) parameter
-	after the last found FROM (RELAY_FROM) parameter. Notice that in most
-	cases the incoming I1 has no paramaters at all, and this "while" loop
-	is skipped. Multiple rvses en route to responder is one (and only?)
-	case when the incoming I1 packet has parameters. */
-     while ((current_param = hip_get_next_param(i1, current_param)) != NULL){
-		
-	  HIP_DEBUG("Found parameter in I1.\n");
-	  /* Copy while type is smaller than or equal to FROM (RELAY_FROM)
-	     or a new FROM (RELAY_FROM) has already been added. */
-	  if (from_added || hip_get_param_type(current_param) <= param_type)
-	  {
-	       HIP_DEBUG("Copying existing parameter to I1 packet "\
-			 "to be relayed.\n");
-	       hip_build_param(i1_to_be_relayed,current_param);
-	       continue;
-	  }
-	  /* Parameter under inspection has greater type than FROM
-	     (RELAY_FROM) parameter: insert a new FROM (RELAY_FROM) parameter
-	     between the last found FROM (RELAY_FROM) parameter and
-	     "current_param". */
-	  else
-	  {
-	       HIP_DEBUG("Created new %s and copied "\
-			 "current parameter to relayed I1.\n",
-			 hip_param_type_name(param_type));
-	       builder_function(i1_to_be_relayed, i1_saddr,
-				i1_info->src_port);
-	       hip_build_param(i1_to_be_relayed, current_param);
-	       from_added = 1;
-	  }
-     }
-
-     /* If the incoming I1 had no parameters after the existing FROM (RELAY_FROM)
-	parameters, new FROM (RELAY_FROM) parameter is not added until here. */
-     if (!from_added)
-     {
-	  HIP_DEBUG("No parameters found, adding a new %s.\n",
-		    hip_param_type_name(param_type));
-	  builder_function(i1_to_be_relayed, i1_saddr, i1_info->src_port);
-     }
-
-     /* Zero message HIP checksum. */
-     hip_zero_msg_checksum(i1_to_be_relayed);
-
-     /* Adding RVS_HMAC parameter as the last parameter of the relayed
-	packet. Notice, that this presumes that there are no parameters
-	whose type value is greater than RVS_HMAC in the incoming I1
-	packet. */
-     HIP_DEBUG("Adding a new RELAY_HMAC parameter as the last parameter.\n");
-     HIP_IFEL(hip_build_param_full_relay_hmac_contents(i1_to_be_relayed,
-						&(rec->hmac_relay)), -1,
-	      "Building of RVS_HMAC failed.\n");
-	
-     /* If the client is behind NAT the I1 packet is relayed on UDP. If
-	there is no NAT the packet is relayed on raw HIP. We don't have to
-	take care of which send-function to use, as the rec->send_fn was
-	initiated with correct value when the relay relay was created. Note
-	that we use NULL as source IP address instead of
-	i1_daddr. A source address is selected in the corresponding
-	send-function. */
-     HIP_IFEL(rec->send_fn(NULL, &(rec->ip_r), HIP_NAT_UDP_PORT,
-			   rec->udp_port_r, i1_to_be_relayed, NULL, 0),
-	      -ECOMM, "Relaying I1 failed.\n");
-
-     /* Once we have relayed the I1 packet successfully, we update the time of
-	last contact. */
-     rec->last_contact = time(NULL);
-
-     HIP_DEBUG_HIT("hip_relay_full(): Relayed I1 to", &(rec->ip_r));
-
- out_err:
-     if(i1_to_be_relayed != NULL)
-     {
-	  HIP_FREE(i1_to_be_relayed);
-     }
-     return err;
-}
-
-
-
-/**
- * 
- * 
- * 
- */
-int hip_relay_response(const hip_common_t *r,const uint8_t type_hdr, const in6_addr_t *r_saddr,
-		  const in6_addr_t *r_daddr , const hip_portpair_t *r_info , 
-		  const in6_addr_t *relay_to_addr,const in_port_t relay_to_port)
-{
-     struct hip_common *r_to_be_relayed = NULL;
-     struct hip_tlv_common *current_param = NULL;
-     int err = 0;
-     hip_tlv_type_t param_type = 0;
-
-
-
-     HIP_DEBUG("hip_relay_response() invoked.\n");
-     HIP_DEBUG_IN6ADDR("hip_relay_response():  source address", r_saddr);
-     HIP_DEBUG_IN6ADDR("hip_relay_response():  destination address", r_daddr);
-     HIP_DEBUG_IN6ADDR("hip_relay_response():  relay to address", relay_to_addr);
-     //HIP_DEBUG_HIT("hip_relay_full: Relay record hit", &rec->hit_r);
-     HIP_DEBUG("Relay_to port: %d.\n", relay_to_port);
-
-
-
-
-     HIP_IFEL(!(r_to_be_relayed = hip_msg_alloc()), -ENOMEM,
-	      "No memory to copy original I1\n");	
-
-
-     hip_build_network_hdr(r_to_be_relayed, type_hdr, 0,
-			   &(r->hits), &(r->hitr));
-
-
-     while ((current_param = hip_get_next_param(r, current_param)) != NULL){
-		
-		HIP_DEBUG("Found parameter in R.\n");
-
-		HIP_DEBUG("Copying existing parameter to R packet "\
-		 "to be relayed.\n");
-		hip_build_param(r_to_be_relayed,current_param);
-
-     }
-
-
-     hip_zero_msg_checksum(r_to_be_relayed);
-
-     
-
-     HIP_IFEL(hip_send(NULL, relay_to_addr, HIP_NAT_UDP_PORT,
-			   relay_to_port, r_to_be_relayed, NULL, 0),
-	      -ECOMM, "Relaying I1 failed.\n");
-
-
-
-     HIP_DEBUG_HIT("hip_relay_response(): Relayed  to", relay_to_addr);
-
- out_err:
-     if(r_to_be_relayed != NULL)
-     {
-	  HIP_FREE(r_to_be_relayed);
-     }
-     return err;
-}
-
-
 int hip_relay_handle_from(hip_common_t *source_msg,
 			  in6_addr_t *rvs_ip,
-			  in6_addr_t *dest_ip, in_port_t *dest_port, hip_tlv_type_t *param_type)
+			  in6_addr_t *dest_ip, in_port_t *dest_port)
 {
 	struct hip_relay_from *relay_from = NULL;
 	struct hip_from *from = NULL;
