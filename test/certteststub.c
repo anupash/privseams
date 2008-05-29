3cacc02c2b7ed1e820b499c0eac8ca50e7658eeb
Samu Varjonen <samu.varjonen@helsinki.fi>
2008-05-29 13:32:56 +0000

    Added zlib compression test to certteststub

--- test/certteststub.c
+++ test/certteststub.c
@@ -13,6 +13,7 @@
  */
 #include <sys/time.h>
 #include <time.h>
+#include <zlib.h>
 #include "ife.h"
 #include "icomm.h"
 #include "debug.h"
@@ -28,10 +29,16 @@ int main(int argc, char *argv[])
         struct in6_addr *defhit;
         struct hip_tlv_common *current_param = NULL;
         struct endpoint_hip *endp = NULL;
-        char temporary[256];
+        char certificate[1024];
+        char compressed[1024];
+        char uncompressed[1024];
+        int return_value = 0;
+        uLongf compressed_length = 0;
+        uLongf uncompressed_length = 0;
        
         HIP_DEBUG("Starting to test SPKI certficate tools\n");
-        HIP_DEBUG("Hipd has to run otherwise this will hang\n");
+        HIP_DEBUG("- This test tool has to be run as root otherwise this will fail!\n") ;
+        HIP_DEBUG("- Hipd has to run otherwise this will hang!\n");
         
         cert = malloc(sizeof(struct hip_cert_spki_info));
         if (!cert) goto out_err;
@@ -45,7 +52,9 @@ int main(int argc, char *argv[])
         time(&not_before);
         time(&not_after);
 
-        /* get the first RSA HIT */
+        /* 
+           get the first RSA HIT 
+        */
         HIP_IFEL(hip_build_user_hdr(msg, SO_HIP_GET_HITS,0),-1, "Fail to get hits");
         hip_send_recv_daemon_info(msg);
 	
@@ -82,17 +91,59 @@ int main(int argc, char *argv[])
            and create a hip_cert_spki_info and send it to the daemon 
            for verification.
         */
-        memset(&temporary, '\0', sizeof(temporary));
-        sprintf(&temporary,"(sequence %s%s%s)", 
+        memset(&certificate, '\0', sizeof(certificate));
+        sprintf(&certificate,"(sequence %s%s%s)", 
                 cert->public_key, cert->cert, cert->signature);
         HIP_DEBUG("\n\nCertificate gotten back from daemon:\n\n"
                   "%s\n\nCertificate len %d\n\n",
-                  temporary, strlen(temporary));
+                  certificate, strlen(certificate));
 
-        HIP_IFEL(hip_cert_spki_char2certinfo(temporary, to_verification), -1,
+        /* 
+           Test if Zlib can compress the certificate into smaller space
+        */
+        HIP_DEBUG("Testing Zlib compression on the certificate");
+        memset(&compressed, '\0', sizeof(compressed));
+        memset(&uncompressed, '0', sizeof(uncompressed));
+        compressed_length = sizeof(compressed);
+        _HIP_DEBUG("Destination length %d\n", compressed_length);
+      
+        return_value = compress2((Bytef *)compressed , &compressed_length, 
+                                (Bytef *)&certificate, (uLong)strlen(certificate),
+                                 Z_BEST_COMPRESSION);
+        if (return_value == Z_OK) HIP_DEBUG("Compression was succesfull\n");
+        if (return_value == Z_BUF_ERROR) 
+                HIP_DEBUG("Compression was NOT succesfull (given buffer is too small)\n");
+        if (return_value == Z_MEM_ERROR) 
+                HIP_DEBUG("Compression was NOT succesfull (not enough memory)\n");
+        HIP_DEBUG("Compressed certificate length: %d\n", compressed_length);        
+        
+        uncompressed_length = sizeof(uncompressed);
+        return_value = uncompress((Bytef *)uncompressed, &uncompressed_length,
+                                  (Bytef *)compressed, (uLong)compressed_length);
+        if (return_value == Z_OK) HIP_DEBUG("Uncompression was succesfull\n");
+        if (return_value == Z_BUF_ERROR) 
+                HIP_DEBUG("Uncompression was NOT succesfull (given buffer is too small)\n");
+        if (return_value == Z_MEM_ERROR) 
+                HIP_DEBUG("Uncompression was NOT succesfull (not enough memory)\n");        
+ 
+        uncompressed[strlen(certificate)] = '\0';
+        _HIP_DEBUG("Uncompressed:\n%s\n\n",uncompressed);
+        if (!strcmp(certificate, uncompressed))
+                HIP_DEBUG("Uncompressed certificate did match the original\n\n");
+        else
+                HIP_DEBUG("Uncompressed certificate did NOT match the original\n\n");
+
+
+        /* 
+           end of compression testing 
+        */
+
+        HIP_IFEL(hip_cert_spki_char2certinfo(certificate, to_verification), -1,
                  "Failed to construct the hip_cert_spki_info from certificate\n");
 
-        /* Send the cert to the daemon for verification */
+        /* 
+           Send the cert to the daemon for verification 
+        */
         HIP_DEBUG("Sending the certificate to daemon for verification\n");
 
         HIP_IFEL(hip_cert_send_to_verification(to_verification), -1,
