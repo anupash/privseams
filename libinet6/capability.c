d9dcc3c8be92bb5015487472aa83eddd6b6c3bb2
Miika Komu <miika@iki.fi>
2008-08-06 11:23:03 +0000

    Another bug fix for hip_set_lowcapability

--- libinet6/capability.c
+++ libinet6/capability.c
@@ -94,16 +94,15 @@ int hip_set_lowcapability(int run_as_nobody) {
 	uid_t ruid,euid;
 	cap_t cap_p;
 	char *cap_s;
-	char *user = USER_NOBODY;
+	char *name;
 	struct passwd *pswd;
-	char name[L_cuserid];
 
 	/* @todo: does this work when you start hipd as root (without sudo) */
-	if (!run_as_nobody)
-	    HIP_IFEL(!(user = getenv("SUDO_USER")), -1,
-			"Failed to determine current username\n");
-
-	memcpy(name, user, strlen(user));
+	if (run_as_nobody)
+		name = USER_NOBODY;
+	else
+		HIP_IFEL(!(name = getenv("SUDO_USER")), -1,
+			 "Failed to determine current username\n");
 
 	HIP_IFEL(prctl(PR_SET_KEEPCAPS, 1), -1, "prctl err\n");
 
