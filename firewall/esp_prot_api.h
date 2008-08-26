7a4b1f6a2c830385dbbbb24b598cb9ef827f7173
Rene Hummen <rene.hummen@rwth-aachen.de>
2008-08-26 09:24:25 +0000

    removed uneccessary #if 0 block

--- firewall/esp_prot_api.h
+++ firewall/esp_prot_api.h
@@ -56,16 +56,6 @@ hash_function_t esp_prot_get_hash_function(uint8_t transform);
 int esp_prot_get_hash_length(uint8_t transform);
 hash_chain_t * esp_prot_get_bex_hchain_by_anchor(unsigned char *hchain_anchor,
 		uint8_t transform);
-
-#if 0
-int esp_prot_set_sadb(hip_sa_entry_t *entry, uint8_t esp_prot_transform,
-		unsigned char *esp_prot_anchor, int direction);
-int add_esp_prot_hash(unsigned char *out_hash, uint16_t *out_length, hip_sa_entry_t *entry);
-int verify_esp_prot_hash(hip_sa_entry_t *entry, unsigned char *hash_value);
-hash_chain_t * esp_prot_get_corresponding_hchain(unsigned char *hchain_anchor,
-		uint8_t transform);
-#endif
-
 int esp_prot_get_data_offset(hip_sa_entry_t *entry);
 int esp_prot_sadb_maintenance(hip_sa_entry_t *entry);
 
