06cb0ae560987069680f4fc4a8cbe1188700a351
Rene Hummen <rene.hummen@rwth-aachen.de>
2008-09-09 16:34:22 +0000

    changed size of both hchains

--- firewall/esp_prot_api.c
+++ firewall/esp_prot_api.c
@@ -17,8 +17,8 @@ static const int hash_lengths[NUM_HASH_FUNCTIONS][NUM_HASH_LENGTHS]
 				   = {{8, 16, 20}, {8, 16, 0}};
 
 
-static const int bex_hchain_length = 200000;
-static const int update_hchain_lengths[NUM_UPDATE_HCHAIN_LENGTHS] = {1000};
+static const int bex_hchain_length = 100000;
+static const int update_hchain_lengths[NUM_UPDATE_HCHAIN_LENGTHS] = {100000};
 
 
 /* stores the mapping transform_id -> (function_id, hash_length_id)
