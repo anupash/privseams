d2c0c43dafd1ba9bdbfc76cf850485d002943d1b
Miika Komu <miika@iki.fi>
2008-09-18 05:16:49 +0000

    Bug id 621 (hip udp encapsulation on 64-bit machines or newer kernels)

--- hipd/output.c
+++ hipd/output.c
@@ -1346,6 +1346,8 @@ int hip_send_udp(struct in6_addr *local_addr, struct in6_addr *peer_addr,
 	pkt_info = CMSG_DATA(cmsg);
 	pkt_info->ipi_addr.s_addr = src4.sin_addr.s_addr;
 
+	memset(&hdr, 0, sizeof(hdr)); /* bug id 621 */
+
 	hdr.msg_name = &dst4;
 	hdr.msg_namelen = sizeof(dst4);
 	iov.iov_base = msg;
