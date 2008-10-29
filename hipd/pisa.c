040bd300a3a3152b240f1858400c1b77e7e56570
Thomas Jansen <mithi@mithi.net>
2008-10-29 16:01:36 +0000

    certificate no longer static, but read from file

--- hipd/pisa.c
+++ hipd/pisa.c
@@ -1,18 +1,35 @@
 #ifdef CONFIG_HIP_MIDAUTH
-static char *midauth_cert="(sequence (public_key (rsa-pkcs1-sha1 (e #010001#) \
-(n |n1CheoELqYRSkHYMQddub2TpILl+6H9wC/as6zFCZqOY43hsZgAjG0F\
-GoQwtyOyQjzO2Ykb2TmUCZemTYui/sR0zIbdwg1xafKl7ggZDkhk5an\
-PtGDxJxFalTYo6/A5ZQv8uatbaJgB/G7VM8G+O9HLucadad2zQUXpQf\
-gbK3S8=|)))(cert (issuer (hash hit 2001:0014:06cf:fae7:bb79:bf78:7d64:c056)\
-)(subject (hash hit 2001:0014:06cf:fae7:bb79:bf78:7d64:c056))\
-(not-before \"2008-07-12_22:11:07\")(not-after \"2008-07-22_22:11:07\")\
-)(signature (hash sha1 |kfElDhagiK0Bsqtj32Gq3t/1mxgA|)\
-|HiIqjjZIUzypvoxQyO0UovPm5uC4Xte0scEcBnENDIfn2DNy/bAtxGEdKq4O\
-dW80vTCmkF8/HXclgXLLVch3DxRNdSbYiiks000HpQt/OKqlTH+uUHBcHOAo\
-E42LmDskM9T5KQJoC/CH7871zfvojPnpkl2dUngOWv4q0r/wSJ0=|))";
+
+#include "hipd.h"
+
+#define CERT_MAX_SIZE 1024
+
+static char *midauth_cert = NULL;
+
+int hip_pisa_load_certificate(void)
+{
+	int err = 0;
+	FILE *f = NULL;
+
+	if (midauth_cert)
+		free(midauth_cert);
+	midauth_cert = malloc(CERT_MAX_SIZE);
+	memset(midauth_cert, 0, CERT_MAX_SIZE);
+
+	if (!(f = fopen("/etc/hip/cert", "r"))) {
+		HIP_ERROR("Could not open certificate file.\n");
+		return -1;
+	}
+
+	fread(midauth_cert, CERT_MAX_SIZE - 1, 1, f);
+	fclose(f);
+	return 0;
+}
 
 char *hip_pisa_get_certificate(void)
 {
+	if (!midauth_cert)
+		hip_pisa_load_certificate();
 	return midauth_cert;
 }
 #endif
