d718af0ae2a72f3e72446ebfdca970ea437f7cf5
Blerta Bishaj <blertabishaj@hotmail.com>
2008-10-30 14:56:46 +0000

    Added support for dht queries in the dnsproxy

--- tools/dnsproxy.py
+++ tools/dnsproxy.py
@@ -337,7 +337,7 @@ class Global:
 		if qtype == 28:
                     nam = q1['qname']
                     lr = gp.getbyaaaa(nam)
-		    print lr
+		    #print lr
                     if lr:
                         a2 = {'name': nam,
                               'data': lr,
@@ -380,6 +380,7 @@ class Global:
                             sent_answer = 1
 			except:
 			    fout.write('except aaaa\n')
+			    fout.write('BBBBBBBB  55\n')
 
 		# PTR
                 elif qtype == 12:
@@ -457,9 +458,9 @@ class Global:
 			    s.sendto(m.buf,from_a)
 			    sent_answer = 1
 
-		#-----------------------------------------------
-		# hipconf command execution
-		#-----------------------------------------------
+		#******************************************
+		# 'hipconf dnsproxy ... ' command execution
+		#******************************************
 		#get the PATH env variable
 		cmd = "echo $PATH"
 		p = os.popen(cmd, "r")
@@ -477,9 +478,10 @@ class Global:
 			#fout.write("cmd - %s\n" % (cmd,))
 			p = os.popen(cmd, "r")
 			result = p.readline()
-			#fout.write("RES   %s\n" % (result,))
-			#fout.write("res   %s\n" % (result[len(result)-10:len(result)],))
 			if result[0:3] == "sh:" and result[len(result)-10:len(result)-1] == "not found":
+				#fout.write("not found   %s\n" % (result,))
+				continue;	#try the next command
+			elif result[1:13] == "Check syntax":
 				#fout.write("not found   %s\n" % (result,))
 				continue;	#try the next command
 			else:
@@ -494,8 +496,7 @@ class Global:
 		    p = os.popen(result)
 		#else:
 		#    fout.write('No command - %s\n' % (result,))
-
-		#-----------------------------------------------
+		#***********************************************
 
 		if not sent_answer:
                     s2.send(buf)
@@ -508,6 +509,46 @@ class Global:
 
                 fout.flush()
             except Exception,e:
+		#********************************************
+		# 'hipconf dht get hostname'command execution
+		#********************************************
+		#get the PATH env variable
+		cmd = "echo $PATH"
+		p = os.popen(cmd, "r")
+		line = p.readline()
+
+		#append the PATH env variable
+		line = "/sbin:/usr/sbin/:/usr/local/sbin:" + line[0:len(line) - 1]
+
+		#obtain the commands
+		commands = line.split(':')
+		cmd_worked = 0
+		for c in commands:
+			cmd = c + "/hipconf dht get " + nam + " 3>&1 2>&1 "   #| grep hipconf "
+			p = os.popen(cmd, "r")
+			result = p.readline()
+			if result[0:3] == "sh:" and result[len(result)-10:len(result)-1] == "not found":
+				#fout.write("not found   %s\n" % (result,))
+				continue;	#try the next command
+			else:
+				#fout.write("EXECUTED - %s\n" % (cmd,))
+				fout.write('Result from command: %s\n' % (cmd[0:len(cmd)-10],))
+				fout.write("    %s" % (result,))
+				result = p.readline()
+				while result:
+				    fout.write("   %s" % (result,))
+				    result = p.readline()
+				cmd_worked = 1
+				break;		#this cmd worked
+
+		#add mapping using the hipconf cmd
+		#if cmd_worked == 1:
+		#    result = result + " 1>/dev/null 2>/dev/null 3>/dev/null"
+		#    p = os.popen(result)
+		#else:
+		#    fout.write('No command - %s\n' % (result,))
+		#***********************************************
+
                 fout.write('Exception %s\n' % (e,))
                 
         fout.write('Wants down\n')
