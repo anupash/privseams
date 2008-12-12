02744e8a5a975fdc40fe6bb0570581ef26134888
Miika Komu <miika@iki.fi>
2008-12-12 16:51:04 +0000

    Bug id 707

--- hipd/nsupdate.c
+++ hipd/nsupdate.c
@@ -196,7 +196,7 @@ int run_nsupdate_for_hit (struct hip_host_id_entry *entry, void *opaq)
  */ 
 int nsupdate(const int start)
 {
-	HIP_DEBUG("Updating dns records...");
+	HIP_DEBUG("Updating dns records...\n");
 	hip_for_each_hi(run_nsupdate_for_hit, (void *) &start);
 }
 
