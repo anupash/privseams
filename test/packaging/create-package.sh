c25959d51544235038d0d6214d95aa1caf61fa45
Miika Komu <miika@iki.fi>
2008-12-24 00:49:32 +0000

    Automatized debian repository management

--- test/packaging/create-package.sh
+++ test/packaging/create-package.sh
@@ -10,11 +10,22 @@ SUFFIX="-$VERSION.$RELEASE"
 NAME=hipl
 PKGROOT=$PWD
 PKGEXE=$PKGROOT/test/packaging
+PKG_INDEX_NAME=Packages.gz
+PKG_INDEX_DIR=$PKGEXE
+PKG_INDEX=$PKG_INDEX_DIR/$PKG_INDEX_NAME
 PKGDIR=$PKGROOT/${NAME}$SUFFIX
+PKG_WEB_DIR=
+PKG_SERVER_DIR=
 DEBDIR=/usr/src/debian
 SUBDEBDIRS="BUILD DEBS SOURCES SPECS SDEBS"
 SUDO=sudo
-
+ARCH=
+DISTRO_RELEASE=
+DISTRO=
+DISTROBASE=
+REPO_SERVER=packages.infrahip.net
+REPO_BASE=/var/www/html
+BIN_FORMAT=
 
 die()
 {
@@ -34,6 +45,27 @@ build_rpm()
     # The RPMs can be found from /usr/src/redhat/ SRPMS and RPMS
     $SUDO mv -f $PKGROOT/hipl${SUFFIX}.tar.gz /usr/src/redhat/SOURCES
     $SUDO rpmbuild -ba $SPECFILE
+}
+
+syncrepo_deb()
+{
+    ssh $REPO_SERVER sudo mkdir -p $PKG_SERVER_DIR
+    TEMPDIR=`ssh $REPO_SERVER mktemp -d`
+    scp $PKG_INDEX $REPO_SERVER:$TEMPDIR/
+    scp $PKG_DIR/*.deb $REPO_SERVER:$TEMPDIR/
+    ssh $REPO_SERVER \
+	sudo mv $TEMPDIR/* $PKG_SERVER_DIR/
+    #ssh $REPO_SERVER "chown apache.apache PACKAGES_GZ_DIR"
+}
+
+scanpackages_deb()
+{
+    ORIG=$PWD
+    cd $PKG_DIR
+    WD=`echo $PKG_WEB_DIR|sed 's/\//\\\\\//g'`
+    dpkg-scanpackages . | sed "s/Filename: \./Filename: $WD/" | \
+	gzip -9c > $PKG_INDEX
+    cd $ORIG
 }
 
 build_deb()
@@ -74,7 +106,50 @@ cleanup()
     fi
 }
 
+############### Main program #####################
+
 set -e
+
+# Set architecture, distro and repo details
+if test -r /etc/debian_version
+then
+    DISTROBASE=debian
+    ARCH=`dpkg --print-architecture`
+    PKG_DIR=$DEBDIR/DEBS/$ARCH 
+    DISTRO_RELEASE=`lsb_release -c|cut -f2`
+    DISTRO=`lsb_release -d|cut -f2|tr '[:upper:]' '[:lower:]'|cut -d" " -f1`
+    PKG_WEB_DIR=dists/$DISTRO_RELEASE/main/binary-${ARCH}
+    PKG_SERVER_DIR=$REPO_BASE/$DISTRO/$PKG_WEB_DIR
+elif test -r /etc/redhat-release
+then
+    DISTROBASE=redhat
+    ARCH=`uname -m` # xx test i386
+else
+    die "Unknown architecture"
+fi
+
+# Determine action
+if test x"$1" = x"scanpackages"
+then
+    scanpackages_deb # xx fix rpm
+    exit
+elif test x"$1" = x"syncrepo"
+then
+    syncrepo_deb # xx fix rpm
+    exit
+elif test x"$1" = x"bin"
+then
+    if test test x"$DISTROBASE" = x"redhat"
+    then
+	BIN_FORMAT=rpm
+    elif test x"$DISTROBASE" = x"debian"
+    then
+	BIN_FORMAT=deb
+    else
+	die "Unknown distro"
+    fi
+fi
+echo "Architecture: $ARCH"
 
 echo <<EOF
 ** Creating the directory structure and files for building the
@@ -105,16 +180,17 @@ cat <<EOF
 
 EOF
 
-if test x"$1" = x"rpm"
+if test x"$1" = x"rpm" || test x"$BIN_FORMAT" = x"rpm"
 then
     SPECFILE=$PKGEXE/hipl-rpm.spec 
     build_rpm
-elif test x"$1" = x"deb"
+elif test x"$1" = x"deb" || test x"$BIN_FORMAT" = x"deb"
 then
     SPECFILE=$PKGEXE/hipl-deb.spec 
     build_deb
+    scanpackages_deb
 else
-    echo "*** Unknown platform, aborting ***"
+    die "*** Unknown platform, aborting ***"
 fi
 
 cleanup
