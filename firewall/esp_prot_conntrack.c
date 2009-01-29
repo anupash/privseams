d08304029dbb6cf9d582f52144f7e75a641c43a7
Rene Hummen <rene.hummen@rwth-aachen.de>
2009-01-29 20:45:38 +0000

    hhl testing

--- firewall/esp_prot_conntrack.c
+++ firewall/esp_prot_conntrack.c
@@ -840,6 +840,7 @@ int esp_prot_conntrack_verify(struct esp_tuple *esp_tuple, struct hip_esp *esp)
 		if (err > 0)
 		{
 			HIP_DEBUG("anchor change occurred, handled now\n");
+			printf("anchor change occurred, handled now\n");
 
 			if (use_hash_trees)
 			{
