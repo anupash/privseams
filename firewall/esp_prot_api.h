481fe93b95644c75fcd9892f4abe9a6601b6bac9
Rene Hummen <rene.hummen@rwth-aachen.de>
2009-01-30 21:27:27 +0000

    htree tput

--- firewall/esp_prot_api.h
+++ firewall/esp_prot_api.h
@@ -25,7 +25,7 @@
 #define NUM_UPDATE_HCHAIN_LENGTHS	1
 /* number of hierarchies used to link hchains */
 #define NUM_BEX_HIERARCHIES			1
-#define NUM_UPDATE_HIERARCHIES		300
+#define NUM_UPDATE_HIERARCHIES		1
 
 
 /* maps from the transform_id defined above to the hash-function id
