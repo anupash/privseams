1a46d9f512f1a5c3198072f3cbce9b9ada158ef8
Artturi Karila <artturi.karila.@karila.com>
2009-09-03 10:07:33 +0000

    Bug 890

--- hipd/maintenance.c
+++ hipd/maintenance.c
@@ -1094,6 +1094,7 @@ int verify_hdrr (struct hip_common *msg,struct in6_addr *addrkey)
 	int is_hit_verified  = -1;
 	int is_sig_verified  = -1;
 	int err = 0 ;
+	void *key;
 		
 	hostid = hip_get_param (msg, HIP_PARAM_HOST_ID);
 	if ( addrkey == NULL)
@@ -1120,12 +1121,14 @@ int verify_hdrr (struct hip_common *msg,struct in6_addr *addrkey)
     HIP_IFEL(!(hit_from_hostid = malloc(sizeof(struct in6_addr))), -1, "Malloc for HIT failed\n");
 	switch (alg) {
 		case HIP_HI_RSA:
-			is_sig_verified = hip_rsa_verify(hostid, msg);
+			key = hip_key_rr_to_rsa(hostid, 0);
+			is_sig_verified = hip_rsa_verify(key, msg);
 			err = hip_rsa_host_id_to_hit (hostid, hit_from_hostid, HIP_HIT_TYPE_HASH100);
 			is_hit_verified = memcmp(hit_from_hostid, hit_used_as_key, sizeof(struct in6_addr)) ;
 			break;
 		case HIP_HI_DSA:
-			is_sig_verified = hip_dsa_verify(hostid, msg);
+			key = hip_key_rr_to_dsa(hostid, 0);
+			is_sig_verified = hip_dsa_verify(key, msg);
 			err = hip_dsa_host_id_to_hit (hostid, hit_from_hostid, HIP_HIT_TYPE_HASH100);
 			is_hit_verified = memcmp(hit_from_hostid, hit_used_as_key, sizeof(struct in6_addr)) ; 
 			break;
