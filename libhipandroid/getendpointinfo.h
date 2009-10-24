363621e2b1232b5ad390721d1eddbc614645267a
Joakim Koskela <joakim.koskela@hiit.fi>
2009-10-24 14:32:58 +0000

    Prevented redeclaration in non-android builds

--- libhipandroid/getendpointinfo.h
+++ libhipandroid/getendpointinfo.h
@@ -60,6 +60,7 @@
 #define PF_HIP			32		/* Host Identity Protocol */
 
 /* XX COMMENT ME: WHY THIS RESEMBLES ADDRINFO? */
+#ifdef ANDROID_CHANGES
 struct endpointinfo
 {
   int ei_flags;					/* Input flags.							*/
@@ -71,6 +72,7 @@ struct endpointinfo
   char *ei_canonname;			/* Canonical name for service location. */
   struct endpointinfo *ei_next; /* Pointer to next in list.				*/
 };
+#endif
 
 /* Translate the name of a service name to a set of identifiers and locators.*/
 extern int getendpointinfo (__const char *__restrict __nodename,
