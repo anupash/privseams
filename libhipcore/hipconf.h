7ab67d902b2874c756b1c040442c1472940da6d6
Miika Komu <miika@iki.fi>
2009-11-12 14:54:12 +0000

    Minor change to default config

--- libhipcore/hipconf.h
+++ libhipcore/hipconf.h
@@ -223,7 +223,6 @@ opendht on # turn DHT support on (use /etc/hip/dhtservers to define the used ser
 # datapacket on # experimental draft hiccups extensions\n\
 # opp normal|advanced|none\n\
 # transform order 213 # crypto preference order (1=AES, 2=3DES, 3=NULL)\n\
-\n\
 nat plain-udp       # use UDP capsulation (for NATted environments)\n\
 debug medium        # debug verbosity: all, medium or none\n"
 
