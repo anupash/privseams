42f7abc7f5ecd1addd3425a337753013ff9d91c3
Miika Komu <miika@iki.fi>
2009-11-25 08:23:26 +0000

    Bug id 914

--- libhipcore/hashtable.h
+++ libhipcore/hashtable.h
@@ -15,12 +15,42 @@
 #define UP_LOAD		(2*LH_LOAD_MULT) /* load times 256  (default 2) */
 #define DOWN_LOAD	(LH_LOAD_MULT)   /* load times 256  (default 1) */
 
-// XX FIXME: HAS TO BE CONVERTED
-
 #if 0
 static LHASH_HIP *amih;
 static LHASH_HIP *tblhash=NULL;
 static uint reclength=37;
+#endif
+
+/* OpenSSL 1.0.0 introduced backwards incompatible changes to the lhash.
+   These backwards compatibility hacks can be removed when all platforms
+   support OpenSSL 1.0.0 by default. */
+#ifndef LHASH_OF
+#define LHASH_OF(type) struct lhash_st_##type
+#define DECLARE_LHASH_OF(type) LHASH_OF(type) { int dummy; }
+
+#undef IMPLEMENT_LHASH_HASH_FN
+#undef IMPLEMENT_LHASH_COMP_FN
+#undef IMPLEMENT_LHASH_DOALL_FN
+#undef IMPLEMENT_LHASH_DOALL_ARG_FN
+
+#define IMPLEMENT_LHASH_HASH_FN(name, o_type) \
+  unsigned long name##_LHASH_HASH(const void *arg) { \
+  const o_type *a = arg; \
+  return name##_hash(a); }
+#define IMPLEMENT_LHASH_COMP_FN(name, o_type) \
+  int name##_LHASH_COMP(const void *arg1, const void *arg2) { \
+  const o_type *a = arg1;             \
+  const o_type *b = arg2; \
+  return name##_cmp(a,b); }
+#define IMPLEMENT_LHASH_DOALL_FN(name, o_type) \
+  void name##_LHASH_DOALL(void *arg) { \
+  o_type *a = arg; \
+  name##_doall(a); }
+#define IMPLEMENT_LHASH_DOALL_ARG_FN(name, o_type, a_type) \
+  void name##_LHASH_DOALL_ARG(void *arg1, void *arg2) { \
+  o_type *a = arg1; \
+  a_type *b = arg2; \
+  name##_doall_arg(a, b); }
 #endif
 
 //typedef LHASH hip_ht_common;
