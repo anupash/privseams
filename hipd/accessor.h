2a85ecda59867331975b83b60c7db7f8110400f1
Dmitriy Kuptsov <dmitriy.kuptsov@hiit.fi>
2009-12-02 18:45:17 +0000

    bug 959

--- hipd/accessor.h
+++ hipd/accessor.h
@@ -30,12 +30,8 @@ unsigned int hipd_get_state(void);
 void hipd_set_state(unsigned int);
 int hipd_get_flag(unsigned int);
 void hipd_set_flag(unsigned int);
-void hipd_clear_flag(unsigned int);
-
 int hip_agent_is_alive();
 int hip_set_opportunistic_mode(const struct hip_common *msg);
-int hip_get_peer_hit(struct hip_common *msg, const struct sockaddr_un *src);
-int hip_get_pseudo_hit(struct hip_common *msg);
 int hip_query_opportunistic_mode(struct hip_common *msg);
 int hip_query_ip_hit_mapping(struct hip_common *msg);
 int hip_get_hip_proxy_status(void);
