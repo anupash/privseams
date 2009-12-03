88360a74f72b4f74c589d278ef0daa97ab9c3592
Miika Komu <miika@iki.fi>
2009-12-03 23:19:40 +0000

    Minor compilation fixes in firewall.c

--- firewall/firewall.c
+++ firewall/firewall.c
@@ -1800,7 +1800,7 @@ static int hip_fw_init_context(hip_fw_context_t *ctx, const unsigned char *buf, 
 	// length of packet starting at udp header
 	uint16_t udp_len = 0;
 	struct udphdr *udphdr = NULL;
-	int udp_encap_zero_bytes = 0, stun_ret;
+	int udp_encap_zero_bytes = 0;
 
 	// default assumption
 	ctx->packet_type = OTHER_PACKET;
@@ -2051,7 +2051,6 @@ static int hip_fw_init_context(hip_fw_context_t *ctx, const unsigned char *buf, 
 	{
 
 		_HIP_HEXDUMP("stun check failed in UDP",udphdr+1, 20);
-		HIP_DEBUG("stun return is %d \n",stun_ret);
 		HIP_DEBUG("stun len is %d \n",ntohs(udphdr->len) - sizeof(udphdr));
 		/* from the ports and the non zero SPI we can tell that this
 		 * is an ESP packet */
@@ -2440,9 +2439,6 @@ int main(int argc, char **argv){
 		case 'e':
 			hip_userspace_ipsec = 1;
 			hip_esp_protection = 1;
-			break;
-		case 's':
-			hip_stun = 1;
 			break;
 		case 'h':
 			print_usage();
