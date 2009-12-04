8ad0ef9d72e36b1d8dafe3b17c25e94d84a125a1
Miika Komu <miika@iki.fi>
2009-12-04 08:47:29 +0000

    autogen.sh does not invoke configure and make anymore automatically.

--- autogen.sh
+++ autogen.sh
@@ -60,9 +60,9 @@ setup_hipl() {
     echo "Now configuring hipl with default configure options"
     autoreconf --install --force || \
 	(echo "Missing libtool, automake, autoconf or autoreconf?" && exit 1)
-    ./configure $@ || \
-	(echo "Failed to configure hipl" && display_dependencies && exit 1)
-    make
+#    ./configure $@ || \
+#	(echo "Failed to configure hipl" && display_dependencies && exit 1)
+#    make
 }
 
 help() {
@@ -84,3 +84,5 @@ display_pre_info
 
 setup_hipl $@ && display_post_info
 display_kernel_info
+echo ""
+echo "If there were no errors above, run now: ./configure && make"
