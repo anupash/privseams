73e72a069b491791a76ac2492dcc21a99e748e02
Baris Boyvat <baris@boyvat.com>
2009-12-06 20:16:06 +0000

    Doxygen comments for update.h

--- hipd/update.h
+++ hipd/update.h
@@ -12,15 +12,31 @@
 #include "builder.h"
 #include "hadb.h"
 
+/**
+ * Sends all the locators from our active source address to the active
+ * destination addresses of all peers.
+ *
+ * Notice that the update packet is sent between only one active address pair
+ * between two peers. When shotgun is implemented this will change.
+ *
+ * @return 0 if succeeded, error number otherwise
+*/
 int hip_send_locators_to_all_peers();
 
-int hip_send_locators_to_one_peer(hip_common_t* received_update_packet,
-        struct hip_hadb_state *ha, struct in6_addr *src_addr,
-        struct in6_addr *dst_addr, struct hip_locator_info_addr_item *locators,
-        int type);
-
+/**
+ * Handles a received update packet.
+ *
+ * @param msg: received update packet
+ * @param src_addr: source address from which this received update packet was sent
+ * @param dst_addr: destination address to which this received update packet was sent
+ * @param ha: corresponding host association between the peers update packets was
+ *  transmitted
+ * @param sinfo: port information for the received update packet
+ *
+ * @return 0 if succeeded, error number otherwise
+ */
 int hip_receive_update(hip_common_t* msg, in6_addr_t *src_addr,
-        in6_addr_t *dst_addr, hip_ha_t *entry, hip_portpair_t *sinfo);
+        in6_addr_t *dst_addr, hip_ha_t *ha, hip_portpair_t *sinfo);
 
 
 #endif /* HIP_UPDATE_H */
