969539c88d0b4b7b94d1d764ffd560bf9e2a1f71
Miika Komu <miika@iki.fi>
2009-12-29 15:02:17 +0000

    Bug id 1226 Rene Hummen: I didn't have the time to check it, but the attached patch should actually do the trick.

--- libhiptool/nlink.h
+++ libhiptool/nlink.h
@@ -8,7 +8,7 @@
 
 #include "libhipcore/builder.h"
 #include "libhipcore/debug.h"
-#include "xfrm.h"
+#include <linux/xfrm.h>
 
 /* Keep this one as the last to avoid some weird compilation problems */
 #include <linux/netlink.h>
