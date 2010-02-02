48f7beb8d426fba758c513f24bd5a7022c6c970c
Tim Just <tim.just@rwth-aachen.de>
2010-02-02 08:55:10 +0100

    Added example file for module initialization and adjusted .bzrignore.

---
+++ modules/update/hipd/update_init.h
@@ -0,0 +1,13 @@
+/*
+ * update_init.h
+ *
+ *  Created on: Jan 29, 2010
+ *      Author: just
+ */
+
+#ifndef UPDATE_INIT_H_
+#define UPDATE_INIT_H_
+
+int update_init(void);
+
+#endif /* UPDATE_INIT_H_ */
