c77b09e905ebbfd0f9d1a466d960ae079857ab23
Tobias Heer <heer@cs.rwth-aachen.de>
2010-03-03 15:56:42 +0100

    Doxygen for hip_statistics.*

--- lib/core/hip_statistics.h
+++ lib/core/hip_statistics.h
@@ -1,3 +1,11 @@
+/** @file hip_statistics.h
+ *
+ * Distributed under <a href="http://www.gnu.org/licenses/gpl2.txt">GNU/GPL</a>
+ *
+ * Header file for hip_statistics.c
+ *
+ * @author Rene Hummen
+ */
 #ifndef HIP_LIB_CORE_HIP_STATISTICS_H
 #define HIP_LIB_CORE_HIP_STATISTICS_H
 
@@ -8,12 +16,15 @@
 #define STATS_IN_MSECS  1000
 #define STATS_IN_USECS  1000000
 
+/**
+ * Data set that contains the the collected values
+ */
 typedef struct statistics_data {
-    uint32_t num_items;
-    uint64_t added_values;
-    uint64_t added_squared_values;
-    uint64_t min_value;
-    uint64_t max_value;
+    uint32_t num_items;             /* number of items that have been added to the set */
+    uint64_t added_values;          /* total amount of added values */
+    uint64_t added_squared_values;  /* squared values for standard deviation calculation */
+    uint64_t min_value;             /* minimal of all values added to the set */
+    uint64_t max_value;             /* maximum of all values added to the set */
 } statistics_data_t;
 
 uint64_t calc_timeval_diff(const struct timeval *timeval_start,
