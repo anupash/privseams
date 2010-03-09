461ab16d0b3696ee95522e2676543a3fc198cb19
Mircea Gherzan <mircea.gherzan@rwth-aachen.de>
2010-03-09 16:29:33 +0100

    Fix build with midauth and OpenWRT enabled.

--- lib/core/filemanip.h
+++ lib/core/filemanip.h
@@ -10,6 +10,7 @@
 int hip_create_lock_file(char *filename, int killold);
 int hip_remove_lock_file(char *filename);
 int check_and_create_dir(char *dirname, mode_t mode);
+int check_and_create_file(char *filename, mode_t mode);
 void change_key_file_perms(char *filenamebase);
 
 #endif /* HIP_LIB_CORE_FILEMANIP_H */
