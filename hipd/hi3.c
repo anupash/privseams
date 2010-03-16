77d0de8cfd53e98740cb831a5edebd583041c49d
Miika Komu <mkomu@halko>
2010-03-16 16:35:23 +0200

    Added doxygen for hi3. Submitted by Andrey Lukyanenko.

--- hipd/hi3.c
+++ hipd/hi3.c
@@ -1,3 +1,17 @@
+/** @file
+ *
+ * Distributed under <a href="http://www.gnu.org/licenses/gpl2.txt">GNU/GPL</a>
+ *
+ * HIP-based Internet Indirection Infrastructure (Hi3) code. This code
+ * can be used to relay HIP-based traffic over i3 infrastructure.
+ * See <a href="http://www.tml.tkk.fi/~pnr/publications/sncnw2004.pdf">
+ * Nikander et al, Host Identity Indirection Infrastructure (Hi3), Nov 2004</a>
+ * or <a href="http://tools.ietf.org/html/draft-nikander-hiprg-hi3">
+ * Nikander et al, Host Identity Indirection Infrastructure (Hi3),
+ * June 2004, expired Internet draft</a>.
+ *
+ * @author Andrey Lukyanenko
+ */
 /* required for s6_addr32 */
 #define _BSD_SOURCE
 
@@ -20,9 +34,9 @@ static int hip_hi3_insert_trigger(void);
 /**
  * The callback for i3 "no matching id" callback.
  *
- * @param ctx_data a pointer to...
- * @param data     a pointer to...
- * @param fun_ctx  a pointer to...
+ * @param ctx_data a pointer to hi3 ID
+ * @param data     a pointer to additional data
+ * @param fun_ctx  a pointer to function's context information (not used)
  * @todo           tkoponen: should this somehow trigger the timeout for waiting
  *                 outbound traffic (state machine)?
  */
@@ -34,6 +48,12 @@ static void no_matching_trigger(void *ctx_data, void *data, void *fun_ctx)
     HIP_ERROR("Following ID not found: %s\n", id);
 }
 
+
+/**
+ * hi3 initializing function for hip (called from main module)
+ *
+ * @return 0 on success, terminates program on i3 error
+ */
 int hip_i3_init()
 {
     if (cl_init(HIPL_HI3_FILE) != CL_RET_OK) {
@@ -49,12 +69,19 @@ int hip_i3_init()
     return 0;
 }
 
+/**
+ * Adds trigger from defined hip_host_id_entry structure to the list of the available triggers hi3_pub_id
+ *
+ * @param entry    a pointer to hip_host_id_entry structure
+ * @param count    a pointer to the number of triggers in hi3_pub_id array
+ * @return 0 on success, negative on the trigger number exceeded
+ */
 static int hip_hi3_add_pub_trigger_id(struct hip_host_id_entry *entry, void *count)
 {
     int i = *(int *) count;
     if (i > HI3_TRIGGER_MAX) {
         HIP_ERROR("Trigger number exceeded");
-        return 0;
+        return -1;
     }
 
     bzero(&hi3_pub_id[i], ID_LEN);
@@ -66,6 +93,10 @@ static int hip_hi3_add_pub_trigger_id(struct hip_host_id_entry *entry, void *cou
 
 /**
  * This is the i3 callback to process received data.
+ *
+ * @param t  a pointer to cl_trigger (not used right now)
+ * @param data a pointer to data received (including padding in the start and end, headers, payload).
+ * @param fun_ctx  a pointer to function's context information (not used)
  */
 static void hip_hi3_receive_payload(void *t, void *data, void *fun_ctx)
 {
@@ -120,8 +151,12 @@ out_err:
     ;
 }
 
-/*
- * i3 callbacks for trigger management
+/**
+ * i3 callbacks for trigger management (information output)
+ *
+ * @param t a pointer to cl_trigger struct
+ * @param data     a pointer to additional data
+ * @param fun_ctx  a pointer to function's context
  */
 static void hip_hi3_constraint_failed(void *t, void *data, void *fun_ctx)
 {
@@ -129,6 +164,13 @@ static void hip_hi3_constraint_failed(void *t, void *data, void *fun_ctx)
     HIP_ERROR("Trigger constraint failed\n");
 }
 
+/**
+ * i3 callbacks for trigger insertion success
+ *
+ * @param t a pointer to cl_trigger struct
+ * @param data     a pointer to additional data
+ * @param fun_ctx  a pointer to function's context
+ */
 static void hip_hi3_trigger_inserted(void *t, void *data, void *fun_ctx)
 {
     char id[100];
@@ -136,9 +178,17 @@ static void hip_hi3_trigger_inserted(void *t, void *data, void *fun_ctx)
 
     sprintf_i3_id(id, &type->t->id);
 
+    // it should not be error -> info?
     HIP_ERROR("Trigger inserted: %s\n", id);
 }
 
+/**
+ * i3 callbacks for trigger insert failure
+ *
+ * @param t a pointer to cl_trigger struct
+ * @param data     a pointer to additional data
+ * @param fun_ctx  a pointer to function's context
+ */
 static void hip_hi3_trigger_failure(void *t, void *data, void *fun_ctx)
 {
     cl_trigger *type = (cl_trigger *) t;
@@ -150,6 +200,12 @@ static void hip_hi3_trigger_failure(void *t, void *data, void *fun_ctx)
     cl_insert_trigger(type, 0);
 }
 
+/**
+ * The function to insert hi3 specific triggers into i3 network. It defines all callback functions for every new trigger.
+ * All triggers IDs are selected from array hi3_pri_id and hi3_pub_id.
+ *
+ * @return 0 always
+ */
 static int hip_hi3_insert_trigger(void)
 {
     Key key[HI3_TRIGGER_MAX];
@@ -204,6 +260,12 @@ static int hip_hi3_insert_trigger(void)
     return 0;
 }
 
+/**
+ * The function cleans the triggers before exit. It destroys all triggers inserted into i3 network
+ * and cleans i3 context information through cl_exit()
+ *
+ * @return 0 always
+ */
 int hip_hi3_clean()
 {
     int i = 0;
@@ -218,6 +280,21 @@ int hip_hi3_clean()
     return 0;
 }
 
+/**
+ * The function makes sure that "right" locators are inserted into HIP i2 packet, because
+ * the hi3 packets goes indirectly through i3 network, both initiator and reponder do not know
+ * about any locators of the other station (use IPv4 or IPv6?). This function ensures that the
+ * IPs are used in the right order (priority to IPv6) and that they are used correctly (without
+ * it HIP assumes that IP addresses already exist on previous stages I1, R1, and it puts address
+ * from database which are zeros).
+ *
+ * @param locator a pointer to hip_locator struct (null if I2 packet missed the field).
+ * @param i2_info     a poinjter to info field of I2 packet
+ * @param i2_saddr     a pointer to I2 source address
+ * @param i2_daddr     a pointer to I2 destination address
+ *
+ * @return 0 always
+ */
 int hip_do_i3_stuff_for_i2(struct hip_locator *locator, hip_portpair_t *i2_info,
                            in6_addr_t *i2_saddr, in6_addr_t *i2_daddr)
 {
