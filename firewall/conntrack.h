d485aaf16f71aeac377c724328d4a53d4fc8379e
Diego Biurrun <diego@biurrun.de>
2010-03-22 00:04:35 +0100

    whitespace cosmetics: fix indentation

--- firewall/conntrack.h
+++ firewall/conntrack.h
@@ -30,13 +30,14 @@ int filter_state(const struct in6_addr *ip6_src,
                  const struct state_option *option,
                  const int accept, hip_fw_context_t *ctx);
 int conntrack(const struct in6_addr *ip6_src,
-               const struct in6_addr *ip6_dst,
-               struct hip_common *buf, hip_fw_context_t *ctx);
+              const struct in6_addr *ip6_dst,
+              struct hip_common *buf, hip_fw_context_t *ctx);
 
 void init_timeout_checking(long int timeout_val);
 
 struct esp_tuple *find_esp_tuple(const SList *esp_list, const uint32_t spi);
-struct tuple *get_tuple_by_hits(const struct in6_addr *src_hit, const struct in6_addr *dst_hit);
+struct tuple *get_tuple_by_hits(const struct in6_addr *src_hit,
+                                const struct in6_addr *dst_hit);
 int hipfw_relay_esp(const hip_fw_context_t *ctx);
 
 #endif
