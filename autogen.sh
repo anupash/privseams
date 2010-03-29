5c6fd91b22d79bb88134bb83d2d84ba7c4199a6e
Diego Biurrun <diego@biurrun.de>
2010-03-30 01:23:58 +0200

    Revert hack that removes dns programs to work around packaging issues.
    I suspect this is no longer a problem in practice. If it is finding
    a proper solution should not be too hard.

--- autogen.sh
+++ autogen.sh
@@ -59,9 +59,6 @@ setup_hipl() {
     autoreconf --install --force || \
         (echo "Missing libtool, automake, autoconf or autoreconf?" && exit 1)
         (echo "" && echo "HIPL has the following dependencies: " && display_dependencies && exit 1)
-    # Clean up generated scripts for "make bin". Otherwise we get
-    # /usr/local/lib/python2.6/dist-packages/ as the path in the scripts.
-    rm -f tools/hipdnsproxy tools/hipdnskeyparse
 }
 
 help() {
