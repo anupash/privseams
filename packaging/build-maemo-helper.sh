84b2ca4dac43ae9e7ebe4107feb0571d8205ef0c
Rene Hummen <rene.hummen@cs.rwth-aachen.de>
2010-05-28 14:04:15 +0200

    reverted revision 4611
    the workaround is no longer needed as autotools find the right python
    interpreter to be run at config time

--- packaging/build-maemo-helper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# HIPL requires Python >= 2.5, this is a workaround to use
-# the installable python2.5 instead of the in-built 2.3.2
-export SBOX_REDIRECT_IGNORE=$SBOX_REDIRECT_IGNORE:/usr/bin/python
-export PATH=/usr/bin:$PATH
-dpkg-buildpackage -rfakeroot -b
