415601d5b6fcb8bc6cd6d02ae860b32f499f3f26
Joakim Koskela <joakim.koskela@hiit.fi>
2010-06-01 18:10:49 +0300

    Removed unused functions and staticized file-internal ones

--- firewall/hslist.c
+++ firewall/hslist.c
@@ -18,34 +18,19 @@
 #include "common_types.h"
 #include "hslist.h"
 
+static SList *slist_last(SList *list);
+
 /**
  * allocate a new linked list element
  *
  * @return the allocated linked list element (caller frees)
  */
-SList *alloc_slist(void)
+static SList *alloc_slist(void)
 {
     SList *list = malloc(sizeof(SList));
     list->next = NULL;
     list->data = NULL;
     return list;
-}
-
-/**
- * deallocate and unlink a linked list element
- *
- * @param list the linked list element to be deallocated
- */
-void free_slist(SList *list)
-{
-    SList *tmp_list = NULL;
-
-    while (list) {
-        tmp_list = list;
-        free(list->data);
-        list     = list->next;
-        free(tmp_list);
-    }
 }
 
 /**
@@ -80,7 +65,7 @@ SList *append_to_slist(SList *list,
  * @param list the linked list to be traversed
  * @return the last element of the linked list
  */
-SList *slist_last(SList *list)
+static SList *slist_last(SList *list)
 {
     if (list) {
         while (list->next) {
@@ -124,34 +109,5 @@ SList *remove_link_slist(SList *list,
         tmp  = tmp->next;
     }
 
-    return list;
-}
-
-/**
- * remove and deallocated an element from the linked list
- *
- * @param list the linked list
- * @param data a pointer to the contents of an element to be removed
- * @return a pointer to the linked list
- */
-SList *remove_from_slist(SList *list,
-                         const void  *data)
-{
-    SList *tmp, *prev = NULL;
-
-    tmp = list;
-    while (tmp) {
-        if (tmp->data == data) {
-            if (prev) {
-                prev->next = tmp->next;
-            } else {
-                list = tmp->next;
-            }
-            free_slist(tmp);
-            break;
-        }
-        prev = tmp;
-        tmp  = prev->next;
-    }
     return list;
 }
--- firewall/hslist.h
+++ firewall/hslist.h
@@ -9,19 +9,10 @@
 
 #include "common_types.h"
 
-SList *alloc_slist(void);
-
-void free_slist(SList *list);
-
 SList *append_to_slist(SList *list,
                        void *data);
 
-SList *remove_from_slist(SList *list,
-                         const void *data);
-
 SList *remove_link_slist(SList *list,
                          SList *link);
-
-SList *slist_last(SList *list);
 
 #endif /* HIP_FIREWALL_HSLIST_H */
