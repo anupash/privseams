a8164ef24a8af762c1105c220a62b3b2cd70eafd
Joakim Koskela <joakim.koskela@hiit.fi>
2010-06-01 18:13:02 +0300

    Removed unused functions and staticized file-internal ones

--- lib/tool/checksum.h
+++ lib/tool/checksum.h
@@ -14,7 +14,6 @@ uint16_t ipv6_checksum(uint8_t protocol,
                        struct in6_addr *dst,
                        void *data, uint16_t len);
 uint16_t checksum_ip(struct ip *ip_hdr, const unsigned int ip_hl);
-unsigned short in_cksum(uint16_t *ptr, int nbytes);
 uint16_t inchksum(const void *data, uint32_t length);
 uint16_t hip_checksum_packet(char *data, struct sockaddr *src, struct sockaddr *dst);
 
