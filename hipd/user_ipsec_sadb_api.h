71d9d4475729ee1bcaaddd0126839f82f0584def
Joakim Koskela <joakim.koskela@hiit.fi>
2010-06-01 18:12:06 +0300

    Removed unused functions and staticized file-internal ones

--- hipd/user_ipsec_sadb_api.h
+++ hipd/user_ipsec_sadb_api.h
@@ -28,31 +28,6 @@ uint32_t hip_userspace_ipsec_add_sa(const struct in6_addr *saddr,
                                     const int direction, const int update,
                                     hip_ha_t *entry);
 
-void hip_userspace_ipsec_delete_sa(const uint32_t spi,
-                                   const struct in6_addr *not_used,
-                                   const struct in6_addr *dst_addr,
-                                   const int direction,
-                                   hip_ha_t *entry);
-
-int hip_userspace_ipsec_flush_all_sa(void);
-
-int hip_userspace_ipsec_setup_hit_sp_pair(const hip_hit_t *src_hit,
-                                          const hip_hit_t *dst_hit,
-                                          const struct in6_addr *src_addr,
-                                          const struct in6_addr *dst_addr,
-                                          const uint8_t proto,
-                                          const int use_full_prefix,
-                                          const int update);
-
-void hip_userspace_ipsec_delete_hit_sp_pair(const hip_hit_t *src_hit,
-                                            const hip_hit_t *dst_hit,
-                                            const uint8_t proto,
-                                            const int use_full_prefix);
-
-int hip_userspace_ipsec_flush_all_policy(void);
-
-void hip_userspace_ipsec_delete_default_prefix_sp_pair(void);
-
 int hip_userspace_ipsec_setup_default_sp_prefix_pair(void);
 
 #endif /*HIP_HIPD_USER_IPSEC_SADB_API_H*/
