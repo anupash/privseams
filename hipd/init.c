d4cdeb2e51305ed34e673cac8020c996c04a6c37
Mircea Gherzan <mircea.gherzan@rwth-aachen.de>
2010-06-15 12:55:55 +0200

    Remove two kernel modules from the list. "Normal" tunneling (IPIP, 4in6, etc)  is not needed for the moment, as long as BEET mode is in place.

--- hipd/init.c
+++ hipd/init.c
@@ -267,7 +267,6 @@ static const char *kernel_crypto_mod[] = {
 /** Tunneling, IPsec, interface and control modules */
 static const char *kernel_net_mod[] = {
     "xfrm4_mode_beet", "xfrm6_mode_beet",
-    "tunnel4",         "ip6_tunnel",
     "esp4",            "esp6",
     "xfrm_user",       "dummy",
 };
