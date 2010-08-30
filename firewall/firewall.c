e89fd872ccc2ef5598b53251dff9b47de9fb1459
Christof Mroz <christof.mroz@rwth-aachen.de>
2010-08-30 10:28:41 +0200

    Fix revision 4915: indentation, add missing ntohs(), remove temporary variables.

--- firewall/firewall.c
+++ firewall/firewall.c
@@ -1189,10 +1189,10 @@ static int hip_fw_handle_esp_output(hip_fw_context_t *ctx)
  */
 static int hip_fw_handle_other_output(hip_fw_context_t *ctx)
 {
-    HIP_DEBUG("accept_normal_traffic_by_default = %d\n",
-        accept_normal_traffic_by_default);
-
     int verdict = accept_normal_traffic_by_default;
+
+    HIP_DEBUG("accept_normal_traffic_by_default = %d\n",
+              accept_normal_traffic_by_default);
 
     if (ctx->ip_version == 6 && hip_userspace_ipsec) {
 
@@ -1566,13 +1566,12 @@ static int hip_fw_init_context(hip_fw_context_t *ctx,
             // this might be a TCP packet for opportunistic mode
             HIP_DEBUG("plain TCP packet\n");
 
-            struct tcphdr *tcphdr = (struct tcphdr*)
-                (((char *) iphdr) + ip_hdr_len);
             ctx->packet_type       = TCP_PACKET;
-            ctx->transport_hdr.tcp = tcphdr;
+            ctx->transport_hdr.tcp = (struct tcphdr*)
+                                     (((char *) iphdr) + ip_hdr_len);
 
-            HIP_DEBUG("src port: %u\n", ntohs(tcphdr->source));
-            HIP_DEBUG("dst port: %u\n", ntohs(tcphdr->dest));
+            HIP_DEBUG("src port: %u\n", ntohs(ctx->transport_hdr.tcp->source));
+            HIP_DEBUG("dst port: %u\n", ntohs(ctx->transport_hdr.tcp->dest));
 
             goto end_init;
         } else if (iphdr->ip_p != IPPROTO_UDP) {
@@ -1636,13 +1635,12 @@ static int hip_fw_init_context(hip_fw_context_t *ctx,
             // this might be a TCP packet for opportunistic mode
             HIP_DEBUG("plain TCP packet\n");
 
-            struct tcphdr *tcphdr = (struct tcphdr *)
-                    (((char *) ip6_hdr) + sizeof(struct ip6_hdr));
             ctx->packet_type       = TCP_PACKET;
-            ctx->transport_hdr.tcp = tcphdr;
+            ctx->transport_hdr.tcp = (struct tcphdr*)
+                                     (((char *) ip6_hdr) + sizeof(struct ip6_hdr));
 
-            HIP_DEBUG("src port: %u\n", ntohs(tcphdr->source));
-            HIP_DEBUG("dst port: %u\n", ntohs(tcphdr->dest));
+            HIP_DEBUG("src port: %u\n", ntohs(ctx->transport_hdr.tcp->source));
+            HIP_DEBUG("dst port: %u\n", ntohs(ctx->transport_hdr.tcp->dest));
 
             goto end_init;
         } else if (ip6_hdr->ip6_nxt != IPPROTO_UDP) {
@@ -1671,8 +1669,8 @@ static int hip_fw_init_context(hip_fw_context_t *ctx,
         goto end_init;
     }
 
-    HIP_DEBUG("UDP header size  is %d (in header: %u) \n",
-        sizeof(struct udphdr), udphdr->len);
+    HIP_DEBUG("UDP header size is %d (in header: %u) \n",
+              sizeof(struct udphdr), ntohs(udphdr->len));
     HIP_DEBUG("UDP src port: %u\n", ntohs(udphdr->source));
     HIP_DEBUG("UDP dst port: %u\n", ntohs(udphdr->dest));
 
