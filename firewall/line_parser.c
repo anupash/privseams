578353606bc6fd5a6980c8d186fdb446df13c945
Stefan Götz <stefan.goetz@cs.rwth-aachen.de>
2010-10-12 08:39:46 +0200

    Made the small parsing functions inline and moved them to their own header file

--- firewall/line_parser.c
+++ firewall/line_parser.c
@@ -26,30 +26,11 @@
  *
  * @author Stefan Goetz <stefan.goetz@cs.rwth-aachen.de>
  */
-#include <string.h>     // memchr()
+#include <stdlib.h>  // calloc()
 
 #include "lib/core/debug.h" // HIP_ASSERT()
-#include "firewall/file_buffer.h"   // hip_lp_new()
+#include "firewall/file_buffer.h"   // hip_fb_new()
 #include "firewall/line_parser.h"
-
-/**
- * A line parser object is used to linearly iterate over the lines in a memory
- * area that holds text.
- * The buffer contents are not modified and the returned line pointers are
- * terminated by \n characters, not \0 characters.
- */
-struct hip_line_parser {
-    /**
-     * The current parsing position.
-     * If NULL, hip_lp_first() needs to be called.
-     * If != NULL, points to the start of line in the memory buffer.
-     */
-    char *cur;
-    /**
-     * The memory buffer this parser operates on.
-     */
-    hip_file_buffer_t *fb;
-};
 
 /**
  * Creates a line parser that can parse the specified file.
@@ -92,69 +73,6 @@ void hip_lp_delete(hip_line_parser_t *lp)
         hip_fb_delete(lp->fb);
     }
     free(lp);
-}
-
-/**
- * Start a new parsing pass with a line parser and return the first line in the
- * buffer.
- * The buffer is not modified and the line is terminated by a new-line
- * character (not a \0 character).
- *
- * A parsing pass consists of starting it via hip_lp_first() and iterating over
- * the lines in the file via hip_lp_next() until it returns NULL.
- *
- * @param lp the line parser to use.
- * @return a pointer to the first line in the file or NULL if no line is
- *  available.
- */
-char *hip_lp_first(hip_line_parser_t *lp)
-{
-    HIP_ASSERT(lp != NULL);
-
-    lp->cur = lp->fb->start;
-
-    return lp->cur;
-}
-
-/**
- * Get the next line in a parsing pass with a line parser.
- *
- * Each invocation of this function returns a pointer to consecutive lines in
- * the buffer to parse.
- * After the last line has been reached, NULL is returned.
- * In that case, parsing can restart by calling hip_lp_first().
- *
- * @param lp the line parser parser to use.
- * @return a pointer to a line in the buffer or NULL if there are no more lines
- *  available.
- */
-char *hip_lp_next(hip_line_parser_t *lp)
-{
-    HIP_ASSERT(lp != NULL);
-
-    // have we reached the end of the buffer in a previous invocation?
-    if (lp->cur != NULL) {
-        size_t remaining;
-
-        // for basic sanity, make sure that lp->cur points somewhere into the buffer
-        HIP_ASSERT(lp->cur >= lp->fb->start && lp->cur < lp->fb->end);
-
-        remaining = lp->fb->end - lp->cur;
-        lp->cur = (char *)memchr(lp->cur, '\n', remaining);
-
-        // given the rest of the parsing code, we should always find a \n, but
-        // let's check to be sure
-        if (lp->cur != NULL) {
-            // cur should not point to the new-line character but to the next one:
-            lp->cur += 1;
-            // is there text on the line here or are we at the end?
-            if (lp->cur >= lp->fb->end) {
-                lp->cur = NULL;
-            }
-        }
-    }
-
-    return lp->cur;
 }
 
 /**
