a397cd96f0e61c8c9f9c3158130fcdc6a986862d
Stefan Götz <stefan.goetz@cs.rwth-aachen.de>
2010-10-16 11:45:49 +0200

    - adopted new format of license statement

--- test/check_lib_core.c
+++ test/check_lib_core.c
@@ -1,6 +1,4 @@
-/**
- * @file
- *
+/*
  * Copyright (c) 2010 Aalto University and RWTH Aachen University.
  *
  * Permission is hereby granted, free of charge, to any person
@@ -23,7 +21,11 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
- *
+ */
+
+/**
+ * @file
+ * @brief Unit tests of lib/core (see doc/HACKING on unit tests).
  * @author Stefan Goetz <stefan.goetz@cs.rwth-aachen.de>
  */
 #include <stdlib.h>
