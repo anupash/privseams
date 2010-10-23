a2935551518ce9374badca85a5398b0d6e119e65
Stefan Götz <stefan.goetz@cs.rwth-aachen.de>
2010-10-23 10:57:21 +0200

    Simplified resource de-allocation with a call to hip_port_bindings_uninit()

--- firewall/port_bindings.c
+++ firewall/port_bindings.c
@@ -386,9 +386,7 @@ int hip_port_bindings_init(const bool enable_cache)
     return 0;
 
 out_err:
-    hip_fb_delete(&udp6_file);
-    hip_fb_delete(&tcp6_file);
-    uninit_cache();
+    hip_port_bindings_uninit();
     return err;
 }
 
