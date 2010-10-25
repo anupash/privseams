e9c57f78ca939729abda0a234d36238523243904
Diego Biurrun <diego@biurrun.de>
2010-10-25 18:09:03 +0200

    Create PKG_DIR before createrepo uses it; otherwise the command fails.

--- packaging/create-package.sh
+++ packaging/create-package.sh
@@ -13,6 +13,7 @@ mkindex_rpm()
     # fix this hack -miika
     test -d  /tmp/hipl-${VERSION}/buildenv/RPMS/i586 &&
         cp -a /tmp/hipl-${VERSION}/buildenv/RPMS/i586 /tmp/hipl-${VERSION}/buildenv/RPMS/i386
+    mkdir -p $PKG_DIR
     createrepo $PKG_DIR
 }
 
