d38cc0496c1ecbc5f0b88024d558b39f108231b0
Henrik Ziegeldorf <henrik.ziegeldorf@rwth-aachen.de>
2010-12-17 12:20:07 +0100

    Added documentation for functions in signaling_prot_common.h. Moved functions around for better structure.

--- modules/signaling/lib/signaling_prot_common.c
+++ modules/signaling/lib/signaling_prot_common.c
@@ -12,99 +12,6 @@
 #include "lib/core/ife.h"
 #include "signaling_prot_common.h"
 
-static void signaling_param_print_field(const char *prefix, const uint16_t length, const unsigned char *p_content) {
-    char buf[length+1];
-
-    if(length == 0) {
-        HIP_DEBUG("%s\t <empty>\n", prefix);
-        return;
-    }
-
-    memset(buf, 0, length + 1);
-    memcpy(buf, p_content, length);
-    HIP_DEBUG("%s\t%s\n", prefix, buf);
-}
-
-void signaling_param_appinfo_print(const struct signaling_param_app_context *appinfo) {
-    const uint8_t *p_content;
-
-    if(appinfo == NULL) {
-        HIP_DEBUG("No appinfo parameter given.\n");
-        return;
-    }
-    HIP_DEBUG("+------------ APP INFO START ----------------------\n");
-    HIP_DEBUG("Ports: src %d, dest %d\n", ntohs(appinfo->src_port), ntohs(appinfo->dest_port));
-    p_content = (const uint8_t *) appinfo + sizeof(struct signaling_param_app_context);
-    signaling_param_print_field("Application DN:", ntohs(appinfo->app_dn_length), p_content);
-    p_content += ntohs(appinfo->app_dn_length);
-    signaling_param_print_field("AC Issuer DN:\t", ntohs(appinfo->iss_dn_length), p_content);
-    p_content += ntohs(appinfo->iss_dn_length);
-    signaling_param_print_field("Requirements:\t", ntohs(appinfo->req_length), p_content);
-    p_content += ntohs(appinfo->req_length);
-    signaling_param_print_field("Groups:\t", ntohs(appinfo->grp_length), p_content);
-    HIP_DEBUG("+------------ APP INFO END   ----------------------\n");
-}
-
-void signaling_param_userinfo_print(const struct signaling_param_user_context *userinfo) {
-    const uint8_t *p_content;
-
-    if(userinfo == NULL) {
-        HIP_DEBUG("No userinfo parameter given.\n");
-        return;
-    }
-    p_content = (const uint8_t *) userinfo + sizeof(struct signaling_param_user_context);
-    HIP_DEBUG("+------------ USER INFO START ----------------------\n");
-    signaling_param_print_field("User Name:", ntohs(userinfo->ui_length), p_content);
-    p_content += ntohs(userinfo->ui_length);
-    HIP_HEXDUMP("Signature: ", p_content, ntohs(userinfo->sig_length));
-    HIP_DEBUG("+------------ USER INFO END   ----------------------\n");
-}
-
-int signaling_init_application_context(struct signaling_application_context *app_ctx) {
-    int err = 0;
-
-    HIP_IFEL(!app_ctx, -1, "Application context has to be allocated before initialization\n");
-
-    app_ctx->pid                = -1;
-    app_ctx->application_dn[0]  = '\0';
-    app_ctx->issuer_dn[0]       = '\0';
-    app_ctx->groups[0]          = '\0';
-    app_ctx->requirements[0]    = '\0';
-    app_ctx->path[0]            = '\0';
-
-out_err:
-    return err;
-}
-
-int signaling_init_user_context(struct signaling_user_context *user_ctx) {
-    int err = 0;
-
-    HIP_IFEL(!user_ctx, -1, "User context has to be allocated before initialization\n");
-
-    user_ctx->euid = -1;
-    user_ctx->username[0] = '\0';
-
-out_err:
-    return err;
-}
-
-int signaling_init_connection_context(struct signaling_connection_context *ctx) {
-    int err = 0;
-
-    HIP_IFEL(!ctx, -1, "Connection context has to be allocated before initialization\n");
-
-    ctx->connection_status  = SIGNALING_CONN_NEW;
-    ctx->src_port           = 0;
-    ctx->dest_port          = 0;
-    HIP_IFEL(signaling_init_application_context(&ctx->app_ctx_out),
-             -1, "Could not init outgoing application context\n");
-    HIP_IFEL(signaling_init_user_context(&ctx->user_ctx),
-             -1, "Could not init user context\n");
-
-out_err:
-    return err;
-}
-
 static const char *signaling_connection_status_name(int status) {
     switch (status) {
     case SIGNALING_CONN_NEW:
@@ -120,6 +27,49 @@ static const char *signaling_connection_status_name(int status) {
     }
 }
 
+static void signaling_param_print_field(const char *prefix, const uint16_t length, const unsigned char *p_content) {
+    char buf[length+1];
+
+    if(length == 0) {
+        HIP_DEBUG("%s\t <empty>\n", prefix);
+        return;
+    }
+
+    memset(buf, 0, length + 1);
+    memcpy(buf, p_content, length);
+    HIP_DEBUG("%s\t%s\n", prefix, buf);
+}
+
+/**
+ * Prints the application context parameter.
+ *
+ * @param app the application context parameter to print
+ */
+void signaling_param_appinfo_print(const struct signaling_param_app_context *param_app_ctx) {
+    const uint8_t *p_content;
+
+    if(param_app_ctx == NULL) {
+        HIP_DEBUG("No appinfo parameter given.\n");
+        return;
+    }
+    HIP_DEBUG("+------------ APP INFO START ----------------------\n");
+    HIP_DEBUG("Ports: src %d, dest %d\n", ntohs(param_app_ctx->src_port), ntohs(param_app_ctx->dest_port));
+    p_content = (const uint8_t *) param_app_ctx + sizeof(struct signaling_param_app_context);
+    signaling_param_print_field("Application DN:", ntohs(param_app_ctx->app_dn_length), p_content);
+    p_content += ntohs(param_app_ctx->app_dn_length);
+    signaling_param_print_field("AC Issuer DN:\t", ntohs(param_app_ctx->iss_dn_length), p_content);
+    p_content += ntohs(param_app_ctx->iss_dn_length);
+    signaling_param_print_field("Requirements:\t", ntohs(param_app_ctx->req_length), p_content);
+    p_content += ntohs(param_app_ctx->req_length);
+    signaling_param_print_field("Groups:\t", ntohs(param_app_ctx->grp_length), p_content);
+    HIP_DEBUG("+------------ APP INFO END   ----------------------\n");
+}
+
+/**
+ * Print the internal connection context structure.
+ *
+ * @param ctx the connection context to print
+ */
 void signaling_connection_context_print(const struct signaling_connection_context *ctx) {
     if(ctx == NULL) {
         HIP_DEBUG("No ctx parameter given.\n");
@@ -139,3 +89,93 @@ void signaling_connection_context_print(const struct signaling_connection_contex
     HIP_DEBUG(" \tGroups:\t\t %s\n", ctx->app_ctx_out.groups);
     HIP_DEBUG("+------------ CONNECTION CONTEXT END   ----------------------\n");
 }
+
+/**
+ * Prints the user context parameter.
+ *
+ * @param app the user context parameter to print
+ */
+void signaling_param_userinfo_print(const struct signaling_param_user_context *userinfo) {
+    const uint8_t *p_content;
+
+    if(userinfo == NULL) {
+        HIP_DEBUG("No userinfo parameter given.\n");
+        return;
+    }
+    p_content = (const uint8_t *) userinfo + sizeof(struct signaling_param_user_context);
+    HIP_DEBUG("+------------ USER INFO START ----------------------\n");
+    signaling_param_print_field("User Name:", ntohs(userinfo->ui_length), p_content);
+    p_content += ntohs(userinfo->ui_length);
+    HIP_HEXDUMP("Signature: ", p_content, ntohs(userinfo->sig_length));
+    HIP_DEBUG("+------------ USER INFO END   ----------------------\n");
+}
+
+/**
+ * Initializes the given application context to default values.
+ * Memory for context has to be allocated and freed by the caller.
+ *
+ * @param app_ctx a pointer to the application context that should be initialized
+ *
+ * @return negative value on error, 0 on success
+ */
+int signaling_init_application_context(struct signaling_application_context *app_ctx) {
+    int err = 0;
+
+    HIP_IFEL(!app_ctx, -1, "Application context has to be allocated before initialization\n");
+
+    app_ctx->pid                = -1;
+    app_ctx->application_dn[0]  = '\0';
+    app_ctx->issuer_dn[0]       = '\0';
+    app_ctx->groups[0]          = '\0';
+    app_ctx->requirements[0]    = '\0';
+    app_ctx->path[0]            = '\0';
+
+out_err:
+    return err;
+}
+
+/**
+ * Initializes the given user context to default values.
+ * Memory for context has to be allocated and freed by the caller.
+ *
+ * @param user_ctx a pointer to the user context that should be initialized
+ *
+ * @return negative value on error, 0 on success
+ */
+int signaling_init_user_context(struct signaling_user_context *user_ctx) {
+    int err = 0;
+
+    HIP_IFEL(!user_ctx, -1, "User context has to be allocated before initialization\n");
+
+    user_ctx->euid = -1;
+    user_ctx->username[0] = '\0';
+
+out_err:
+    return err;
+}
+
+/**
+ * Initializes the given connection context to default values.
+ * Memory for context has to be allocated and freed by the caller.
+ *
+ * @param ctx a pointer to the connection context that should be initialized
+ *
+ * @return negative value on error, 0 on success
+ */
+int signaling_init_connection_context(struct signaling_connection_context *ctx) {
+    int err = 0;
+
+    HIP_IFEL(!ctx, -1, "Connection context has to be allocated before initialization\n");
+
+    ctx->connection_status  = SIGNALING_CONN_NEW;
+    ctx->src_port           = 0;
+    ctx->dest_port          = 0;
+    HIP_IFEL(signaling_init_application_context(&ctx->app_ctx_out),
+             -1, "Could not init outgoing application context\n");
+    HIP_IFEL(signaling_init_user_context(&ctx->user_ctx),
+             -1, "Could not init user context\n");
+
+out_err:
+    return err;
+}
+
