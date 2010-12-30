7e86f02602dab8fec3db1fc8c43e613cd34b08e1
Diego Biurrun <diego@biurrun.de>
2010-12-30 22:49:52 +0100

    Fix previous commit that reorganized variable initialization.
    No members of the p structure should be assigned to variables before p
    has been initialized.

--- hipd/cert.c
+++ hipd/cert.c
@@ -964,7 +964,6 @@ int hip_cert_x509v3_handle_request_to_verify(struct hip_common *msg)
     X509_STORE *store          = NULL;
     X509_STORE_CTX *verify_ctx = NULL;
     unsigned char *der_cert    = NULL;
-    const unsigned char *vessel = p->der;
 
     OpenSSL_add_all_algorithms();
     ERR_load_crypto_strings();
@@ -976,6 +975,7 @@ int hip_cert_x509v3_handle_request_to_verify(struct hip_common *msg)
 
     der_cert = (unsigned char *) &p->der;
 
+    const unsigned char *vessel = p->der;
     HIP_IFEL(((cert = d2i_X509(NULL, &vessel, verify.der_len)) == NULL), -1,
              "Failed to convert cert from DER to internal format\n");
     /*
