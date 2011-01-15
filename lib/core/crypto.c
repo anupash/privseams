89b490703e979b435e70a64748af28b18c6cc487
Henrik Ziegeldorf <henrik.ziegeldorf@rwth-aachen.de>
2011-01-15 13:21:02 +0100

    Stricter check for return value of snprintf. Shortened pubfilename_len by one byte, since sizeof already counts null-terminator.

--- lib/core/crypto.c
+++ lib/core/crypto.c
@@ -943,14 +943,14 @@ int save_rsa_private_key(const char *const filenamebase, RSA *const rsa)
     HIP_IFEL(!filenamebase, 1, "NULL filenamebase\n");
 
     pubfilename_len =
-        strlen(filenamebase) + sizeof(DEFAULT_PUB_FILE_SUFFIX) + 1;
+        strlen(filenamebase) + sizeof(DEFAULT_PUB_FILE_SUFFIX);
     pubfilename = malloc(pubfilename_len);
     HIP_IFEL(!pubfilename, 1, "malloc for pubfilename failed\n");
 
     ret = snprintf(pubfilename, pubfilename_len, "%s%s",
                    filenamebase,
                    DEFAULT_PUB_FILE_SUFFIX);
-    HIP_IFEL(ret <= 0, 1, "Failed to create pubfilename\n");
+    HIP_IFEL(ret < pubfilename_len-1, 1, "Failed to create pubfilename\n");
 
     HIP_INFO("Saving RSA keys to: pub='%s' priv='%s'\n", pubfilename,
              filenamebase);
