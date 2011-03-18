d649b622a12d8e191c5b17ef330366de4bff3d0d
Christof Mroz <christof.mroz@rwth-aachen.de>
2011-03-18 15:47:15 +0100

    Update firewall rules according to PISA state changes.

--- firewall/pisa.c
+++ firewall/pisa.c
@@ -311,6 +311,12 @@ static void pisa_accept_connection(const struct hip_fw_context *ctx)
     if (t) {
         t->connection->pisa_state = PISA_STATE_ALLOW;
         HIP_INFO("PISA accepted the connection.\n");
+
+        struct slist *lst = t->esp_tuples;
+        while (lst) {
+            hip_fw_manage_esp_tuple(lst->data, true);
+            lst = lst->next;
+        }
     } else {
         HIP_ERROR("Connection not found.\n");
     }
@@ -329,6 +335,15 @@ static void pisa_remove_connection(const struct hip_fw_context *ctx)
 
     if (t) {
         t->connection->pisa_state = PISA_STATE_DISALLOW;
+        HIP_INFO("PISA removed the connection.\n");
+
+        struct slist *lst = t->esp_tuples;
+        while (lst) {
+            hip_fw_manage_esp_tuple(lst->data, false);
+            lst = lst->next;
+        }
+    } else {
+        HIP_ERROR("Connection not found.\n");
     }
 }
 
