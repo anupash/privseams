487d043c4ac57cbe65a8d1dd61e3112d017cc2c2
Henrik Ziegeldorf <henrik.ziegeldorf@rwth-aachen.de>
2011-07-13 13:12:46 +0200

    Completed doxygen of function dsa_to_hip_endpoint

--- lib/core/builder.c
+++ lib/core/builder.c
@@ -3681,7 +3681,7 @@ int hip_build_param_hit_to_ip_set(struct hip_common *msg, const char *name)
  * @param endpoint An output argument. This function allocates and
  *                 stores the result of the conversion here. Caller
  *                 is responsible of deallocation.
- * @param endpoint_flags
+ * @param endpoint_flags flags for the endpoint
  * @param hostname host name for the DSA key
  * @return zero on success and negative on failure
  */
