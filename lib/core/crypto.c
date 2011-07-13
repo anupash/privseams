a217e8aba17a839b7c6f8ff20c217a9a04ac6eb8
Henrik Ziegeldorf <henrik.ziegeldorf@rwth-aachen.de>
2011-07-13 13:36:20 +0200

    Use HIP_IFEL for if-err-goto code.

--- lib/core/crypto.c
+++ lib/core/crypto.c
@@ -779,12 +779,8 @@ int save_dsa_private_key(const char *const filenamebase, DSA *const dsa)
              "Couldn't open private key file %s for writing\n", filenamebase);
     files++;
 
-    err = PEM_write_DSAPrivateKey(fp, dsa, NULL, NULL, 0, NULL, NULL) == 0 ? 1 : 0;
-
-    if (err) {
-        HIP_ERROR("Write failed for %s\n", filenamebase);
-        goto out_err;
-    }
+    HIP_IFEL(!PEM_write_DSAPrivateKey(fp, dsa, NULL, NULL, 0, NULL, NULL),
+             1, "Write failed for %s\n", filenamebase);
 
 out_err:
     if (err && fp) {
@@ -881,13 +877,8 @@ int save_rsa_private_key(const char *const filenamebase, RSA *const rsa)
              "Couldn't open private key file %s for writing\n", filenamebase);
     files++;
 
-    err = PEM_write_RSAPrivateKey(fp, rsa, NULL, NULL,
-                                  0, NULL, NULL) == 0 ? 1 : 0;
-
-    if (err) {
-        HIP_ERROR("Write failed for %s\n", filenamebase);
-        goto out_err;
-    }
+    HIP_IFEL(!PEM_write_RSAPrivateKey(fp, rsa, NULL, NULL, 0, NULL, NULL),
+             1, "Write failed for %s\n", filenamebase);
 
 out_err:
 
