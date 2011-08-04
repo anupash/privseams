6c6f7617e583beaa672950e3d06bfcf351041ae6
Rene Hummen <rene.hummen@cs.rwth-aachen.de>
2011-08-04 15:33:06 +0200

    complete unit-tests for midauth_builder.c

--- test/modules/midauth/lib/midauth_builder.c
+++ test/modules/midauth/lib/midauth_builder.c
@@ -27,13 +27,153 @@
 #include <stdint.h>
 
 #include "lib/core/protodefs.h"
+#include "modules/midauth/hipd/midauth.h"
 #include "modules/midauth/lib/midauth_builder.h"
 #include "../test_suites.h"
 
 #define MIDAUTH_DEFAULT_NONCE_LENGTH 18
 
+START_TEST(test_midauth_builder_build_param_challenge_request_NULL_msg)
+{
+    const uint8_t opaque[] = "\x01\x41\x01\x14\x05\x00\x48\x49\x0b"
+                             "\x02\x42\x02\x15\x06\x08\x49\x50\x0c";
+
+    fail_unless(hip_build_param_challenge_request(NULL, 0, 0, opaque,
+                                                  MIDAUTH_DEFAULT_NONCE_LENGTH) ==
+                -1, NULL);
+}
+END_TEST
+
 #ifdef HAVE_TCASE_ADD_EXIT_TEST
-START_TEST(test_midauth_builder_puzzle_seed_opaque_NULL)
+START_TEST(test_midauth_builder_build_param_challenge_request_NULL_opaque)
+{
+    struct hip_common *const msg = hip_msg_alloc();
+
+    hip_build_param_challenge_request(msg, 0, 0, NULL,
+                                      MIDAUTH_DEFAULT_NONCE_LENGTH);
+}
+END_TEST
+#endif /* HAVE_TCASE_ADD_EXIT_TEST */
+
+START_TEST(test_midauth_builder_build_param_challenge_request_0_opaque_len)
+{
+    const uint8_t opaque[] = "\x01\x41\x01\x14\x05\x00\x48\x49\x0b"
+                             "\x02\x42\x02\x15\x06\x08\x49\x50\x0c";
+    struct hip_common *const msg = hip_msg_alloc();
+
+    fail_unless(hip_build_param_challenge_request(msg, 0, 0, opaque,
+                                                  0) == 0, NULL);
+}
+END_TEST
+
+START_TEST(test_midauth_builder_build_param_challenge_request_CORRECT)
+{
+    const uint8_t opaque[] = "\x01\x41\x01\x14\x05\x00\x48\x49\x0b"
+                             "\x02\x42\x02\x15\x06\x08\x49\x50\x0c";
+    const char cmp_challenge_request[] = "\xff\x36\x00\x14\x00\x00\x01\x41\x01"
+                                         "\x14\x05\x00\x48\x49\x0b\x02\x42\x02"
+                                         "\x15\x06\x08\x49\x50\x0C";
+    const struct hip_challenge_request *request = NULL;
+    struct hip_common *const msg = hip_msg_alloc();
+
+    fail_unless(hip_build_param_challenge_request(msg, 0, 0, opaque,
+                                                  MIDAUTH_DEFAULT_NONCE_LENGTH) ==
+                0, NULL);
+    fail_unless((request = hip_get_param(msg, HIP_PARAM_CHALLENGE_REQUEST)) != NULL,
+                NULL);
+    fail_unless(memcmp(request, &cmp_challenge_request,
+                       hip_get_param_total_len(request)) == 0, NULL);
+}
+END_TEST
+
+START_TEST(test_midauth_builder_build_param_challenge_response_NULL_msg)
+{
+    const char challenge_request[] = "\xff\x36\x00\x14\x00\x00\x01\x41\x01"
+                                     "\x14\x05\x00\x48\x49\x0b\x02\x42\x02"
+                                     "\x15\x06\x08\x49\x50\x0C";
+    const uint8_t solution[] = "\x01\x41\x01\x14\x05\x00\x48\x49";
+    const struct hip_challenge_request *request =
+            (const struct hip_challenge_request *) challenge_request;
+
+    fail_unless(hip_build_param_challenge_response(NULL, request, solution) == -1,
+                NULL);
+}
+END_TEST
+
+START_TEST(test_midauth_builder_build_param_challenge_response_NULL_request)
+{
+    const uint8_t solution[] = "\x01\x41\x01\x14\x05\x00\x48\x49";
+    const struct hip_challenge_request *request = NULL;
+    struct hip_common *const msg = hip_msg_alloc();
+
+    fail_unless(hip_build_param_challenge_response(msg, request, solution) == -1,
+                NULL);
+}
+END_TEST
+
+START_TEST(test_midauth_builder_build_param_challenge_response_NULL_solution)
+{
+    const char challenge_request[] = "\xff\x36\x00\x14\x00\x00\x01\x41\x01"
+                                     "\x14\x05\x00\x48\x49\x0b\x02\x42\x02"
+                                     "\x15\x06\x08\x49\x50\x0C";
+    const struct hip_challenge_request *request =
+            (const struct hip_challenge_request *) challenge_request;
+    struct hip_common *const msg = hip_msg_alloc();
+
+    fail_unless(hip_build_param_challenge_response(msg, request, NULL) == -1,
+                NULL);
+}
+END_TEST
+
+START_TEST(test_midauth_builder_build_param_challenge_response_CORRECT)
+{
+    const char challenge_request[] = "\xff\x36\x00\x14\x00\x00\x01\x41\x01"
+                                     "\x14\x05\x00\x48\x49\x0b\x02\x42\x02"
+                                     "\x15\x06\x08\x49\x50\x0C";
+    const char cmp_challenge_response[] = "\x01\x42\x00\x1C\x00\x00\x01\x41\x01"
+                                          "\x14\x05\x00\x48\x49\x01\x41\x01\x14"
+                                          "\x05\x00\x48\x49\x0b\x02\x42\x02\x15"
+                                          "\x06\x08\x49\x50\x0c";
+    const uint8_t solution[] = "\x01\x41\x01\x14\x05\x00\x48\x49";
+    const struct hip_challenge_request *request =
+            (const struct hip_challenge_request *) challenge_request;
+    const struct hip_challenge_response *response = NULL;
+    struct hip_common *const msg = hip_msg_alloc();
+
+    fail_unless(hip_build_param_challenge_response(msg, request, solution) == 0,
+                NULL);
+    fail_unless((response = hip_get_param(msg, HIP_PARAM_CHALLENGE_RESPONSE)) != NULL,
+                NULL);
+    fail_unless(memcmp(response, &cmp_challenge_response,
+                       hip_get_param_total_len(response)) == 0, NULL);
+}
+END_TEST
+
+START_TEST(test_midauth_builder_challenge_request_opaque_len_NULL)
+{
+    fail_unless(hip_challenge_request_opaque_len(NULL) == 0, NULL);
+}
+END_TEST
+
+START_TEST(test_midauth_builder_challenge_request_opaque_len_CORRECT)
+{
+    const uint8_t opaque[] = "\x01\x41\x01\x14\x05\x00\x48\x49\x0b"
+                             "\x02\x42\x02\x15\x06\x08\x49\x50\x0c";
+    const struct hip_challenge_request *request = NULL;
+    struct hip_common *const msg = hip_msg_alloc();
+
+    fail_unless(hip_build_param_challenge_request(msg, 0, 0, opaque,
+                                                  MIDAUTH_DEFAULT_NONCE_LENGTH) ==
+                0, NULL);
+    fail_unless((request = hip_get_param(msg, HIP_PARAM_CHALLENGE_REQUEST)) != NULL,
+                NULL);
+    fail_unless(hip_challenge_request_opaque_len(request) ==
+                MIDAUTH_DEFAULT_NONCE_LENGTH, NULL);
+}
+END_TEST
+
+#ifdef HAVE_TCASE_ADD_EXIT_TEST
+START_TEST(test_midauth_builder_puzzle_seed_NULL_opaque)
 {
     const uint8_t *const opaque = NULL;
     uint8_t puzzle_value[PUZZLE_LENGTH];
@@ -43,7 +183,7 @@ START_TEST(test_midauth_builder_puzzle_seed_opaque_NULL)
 END_TEST
 #endif /* HAVE_TCASE_ADD_EXIT_TEST */
 
-START_TEST(test_midauth_builder_puzzle_seed_opaque_len_0)
+START_TEST(test_midauth_builder_puzzle_seed_0_opaque_len)
 {
     const uint8_t opaque[] = "\x01\x41\x00\x14\x05\x00\x48\x49\x0b";
     uint8_t puzzle_value[PUZZLE_LENGTH];
@@ -54,7 +194,17 @@ START_TEST(test_midauth_builder_puzzle_seed_opaque_len_0)
 }
 END_TEST
 
-START_TEST(test_midauth_builder_puzzle_seed_correct)
+START_TEST(test_midauth_builder_puzzle_seed_NULL_puzzle_value)
+{
+    const uint8_t opaque[] = "\x01\x41\x00\x14\x05\x00\x48\x49\x0b";
+
+    fail_unless(hip_midauth_puzzle_seed(opaque,
+                                        MIDAUTH_DEFAULT_NONCE_LENGTH,
+                                        NULL) == -1, NULL);
+}
+END_TEST
+
+START_TEST(test_midauth_builder_puzzle_seed_CORRECT)
 {
     const uint8_t opaque[] = "\x01\x41\x01\x14\x05\x00\x48\x49\x0b"
                              "\x02\x42\x02\x15\x06\x08\x49\x50\x0c";
@@ -74,14 +224,40 @@ Suite *modules_midauth_lib_builder(void)
     Suite *s = suite_create("modules/midauth/lib/midauth_builder");
     TCase *tc_midauth_builder = tcase_create("Midauth_builder");
 
+    tcase_add_test(tc_midauth_builder,
+                   test_midauth_builder_build_param_challenge_request_NULL_msg);
 // the tcase_add_exit_test macro is only available in check 0.9.8 or later but
 // scratchbox uses an older version of checkc so we try to avoid this macro
 #ifdef HAVE_TCASE_ADD_EXIT_TEST
     tcase_add_exit_test(tc_midauth_builder,
-                        test_midauth_builder_puzzle_seed_opaque_NULL, 1);
+                        test_midauth_builder_build_param_challenge_request_NULL_opaque,
+                        1);
 #endif
-    tcase_add_test(tc_midauth_builder, test_midauth_builder_puzzle_seed_opaque_len_0);
-    tcase_add_test(tc_midauth_builder, test_midauth_builder_puzzle_seed_correct);
+    tcase_add_test(tc_midauth_builder,
+                   test_midauth_builder_build_param_challenge_request_0_opaque_len);
+    tcase_add_test(tc_midauth_builder,
+                   test_midauth_builder_build_param_challenge_request_CORRECT);
+    tcase_add_test(tc_midauth_builder,
+                   test_midauth_builder_build_param_challenge_response_NULL_msg);
+    tcase_add_test(tc_midauth_builder,
+                   test_midauth_builder_build_param_challenge_response_NULL_request);
+    tcase_add_test(tc_midauth_builder,
+                   test_midauth_builder_build_param_challenge_response_NULL_solution);
+    tcase_add_test(tc_midauth_builder,
+                   test_midauth_builder_build_param_challenge_response_CORRECT);
+    tcase_add_test(tc_midauth_builder,
+                   test_midauth_builder_challenge_request_opaque_len_NULL);
+    tcase_add_test(tc_midauth_builder,
+                   test_midauth_builder_challenge_request_opaque_len_CORRECT);
+
+#ifdef HAVE_TCASE_ADD_EXIT_TEST
+    tcase_add_exit_test(tc_midauth_builder,
+                        test_midauth_builder_puzzle_seed_NULL_opaque, 1);
+#endif
+    tcase_add_test(tc_midauth_builder, test_midauth_builder_puzzle_seed_0_opaque_len);
+    tcase_add_test(tc_midauth_builder,
+                   test_midauth_builder_puzzle_seed_NULL_puzzle_value);
+    tcase_add_test(tc_midauth_builder, test_midauth_builder_puzzle_seed_CORRECT);
 
     suite_add_tcase(s, tc_midauth_builder);
 
